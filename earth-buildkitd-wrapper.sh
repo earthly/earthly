667cbe0241b73be17cef360694056e7b62c59b8b
Vlad A. Ionescu <446771+vladaionescu@users.noreply.github.com>
2020-03-12 20:15:01 -0700

    Add .circleci/config.yml (#1)
    * Add .circleci/config.yml
    
    * Download earth.
    
    * Executable.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Try again.
    
    * Give up on docker on mac via circleci.
    
    * Some polishing, push docker images.
    
    * Poll for buildkitd readiness.
    
    * -S
    
    * No need to download private GH release files anymore.
    
    Co-authored-by: Vlad A. Ionescu <vladaionescu@users.noreply.github.com>

--- earth-buildkitd-wrapper.sh
+++ earth-buildkitd-wrapper.sh
@@ -2,11 +2,25 @@
 
 # Start buildkitd.
 /usr/bin/entrypoint.sh \
-    buildkitd --allow-insecure-entitlement=security.insecure \
+    buildkitd \
+    --allow-insecure-entitlement=security.insecure \
+    --config=/etc/buildkitd.toml \
     &>/var/log/buildkitd.log \
     &
 buildkitd_pid="$!"
 export EARTHLY_BUILDKIT_HOST="unix:///run/buildkit/buildkitd.sock"
+
+# Poll for buildkitd readiness.
+let i=1
+while [ ! -S "/run/buildkit/buildkitd.sock" ]; do
+    sleep 1
+    let i+=1
+    if [ "$i" -gt "10" ]; then
+        kill -9 "$buildkitd_pid" &>/dev/null
+        echo "Buildkitd did not start within 10 seconds"
+        exit 1
+    fi
+done
 
 # Run earth with given args.
 earth "$@"
