1ee60404254545c2cb6156e39c9fd4feb40e1a3a
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-03-12 20:03:27 -0700

    Poll for buildkitd readiness.

--- earth-buildkitd-wrapper.sh
+++ earth-buildkitd-wrapper.sh
@@ -10,8 +10,17 @@
 buildkitd_pid="$!"
 export EARTHLY_BUILDKIT_HOST="unix:///run/buildkit/buildkitd.sock"
 
-# TODO: Poll for buildkitd readiness.
-sleep 2
+# Poll for buildkitd readiness.
+let i=1
+while [ ! -f "/run/buildkit/buildkitd.sock" ]; do
+    sleep 1
+    let i+=1
+    if [ "$i" -gt "10" ]; then
+        kill -9 "$buildkitd_pid" &>/dev/null
+        echo "Buildkitd did not start within 10 seconds"
+        exit 1
+    fi
+done
 
 # Run earth with given args.
 earth "$@"
