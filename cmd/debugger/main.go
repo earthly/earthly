16944eee9f53de450fa28f7646fd15f69c6f8415
Alex Couture-Beil <alex@earthly.dev>
2020-07-29 10:52:45 -0700

    let the OS provide an open port for the debugger
    Rather than use a hardcoded port, let the OS provide a free port. The
    address to connect to is now passed as a secret to prevent the port from
    affecting the cache.

--- cmd/debugger/main.go
+++ cmd/debugger/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"os"
 	"os/exec"
@@ -54,21 +55,36 @@ func interactiveMode(remoteConsoleAddr string) {
 	}
 }
 
-func main() {
-	remoteConsoleAddr := os.Getenv("EARTHLY_REMOTE_CONSOLE_ADDR")
-	if remoteConsoleAddr == "" {
-		remoteConsoleAddr = "127.0.0.1:8543"
+func getRemoteDebuggerAddr() string {
+	remoteConsoleAddr, err := ioutil.ReadFile("/run/secrets/earthly_remote_console_addr")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Warning: failed to read earthly_remote_console_addr: %v", err)
 	}
+	return string(remoteConsoleAddr)
+}
 
+func main() {
 	args := os.Args[1:]
+
+	remoteConsoleAddr := getRemoteDebuggerAddr()
 
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "command failed: %v; entering debug mode (debugger version %v)\n", err, Version)
-		interactiveMode(remoteConsoleAddr)
-		os.Exit(1)
+		exitCode := 1
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			exitCode = exitErr.ExitCode()
+			fmt.Fprintf(os.Stderr, "Command %v failed with exit code %d", args, exitCode)
+		} else {
+			fmt.Fprintf(os.Stderr, "Command %v failed with unexpected execution error %v", args, err)
+		}
+
+		if remoteConsoleAddr != "" {
+			interactiveMode(remoteConsoleAddr)
+		}
+
+		os.Exit(exitCode)
 	}
 }
