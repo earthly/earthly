9117e794d04d362e150a51d0ce865db9c213d11b
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-08-22 13:30:11 -0700

    Some occasional GC just in case.

--- buildkitd/entrypoint.sh
+++ buildkitd/entrypoint.sh
@@ -22,6 +22,8 @@ if [ "$EARTHLY_RESET_TMP_DIR" == "true" ]; then
     rm -rf "$EARTHLY_TMP_DIR"/* || true
 fi
 
+# clear any leftovers in the dind dir
+rm -rf "$EARTHLY_TMP_DIR/dind"
 mkdir -p "$EARTHLY_TMP_DIR/dind"
 
 # setup git credentials and config
