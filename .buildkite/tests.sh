b62b2e03fd92f9b89d5e292e568116b5d5146912
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-08-29 20:57:37 -0700

    Try again.

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -2,4 +2,20 @@
 
 set -xeuo pipefail
 
-echo Hello wsl
+echo "Downloading latest Earthly binary"
+wget https://github.com/earthly/earthly/releases/latest/download/earth-linux-amd64 -O ./earth-released && chmod +x ./earth-released
+
+echo "Build latest earth using released earth"
+./earth-released +for-linux
+
+echo "Execute tests"
+./build/linux/amd64/earth --no-output -P +test
+
+echo "Execute fail test"
+"! ./build/linux/amd64/earth --no-output +test-fail"
+
+echo "Execute experimental tests"
+./build/linux/amd64/earth --no-output -P ./examples/tests+experimental
+
+echo "Build examples"
+./build/linux/amd64/earth --no-output +examples
