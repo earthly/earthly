314f23c288d4c1fc775aa0ebede81b1a72523e1f
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-08-29 21:06:31 -0700

    Try again.

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -2,7 +2,10 @@
 
 set -xeuo pipefail
 
-echo "Downloading latest Earthly binary"
+echo "Add branch info back to git (Earthly uses it for tagging)"
+git checkout -b "$BUILDKITE_BRANCH" || true
+
+echo "Download latest Earthly binary"
 wget https://github.com/earthly/earthly/releases/latest/download/earth-linux-amd64 -O ./earth-released && chmod +x ./earth-released
 
 echo "Build latest earth using released earth"
@@ -12,7 +15,7 @@ echo "Execute tests"
 ./build/linux/amd64/earth --no-output -P +test
 
 echo "Execute fail test"
-"! ./build/linux/amd64/earth --no-output +test-fail"
+bash -c "! ./build/linux/amd64/earth --no-output +test-fail"
 
 echo "Execute experimental tests"
 ./build/linux/amd64/earth --no-output -P ./examples/tests+experimental
