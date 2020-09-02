798b531bff7620981391880bee09c39f1ee5b4d0
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-09-01 18:11:51 -0700

    Address dind race condition.

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -18,7 +18,7 @@ echo "Execute fail test"
 bash -c "! ./build/$EARTH_OS/amd64/earth --no-output +test-fail"
 
 echo "Execute experimental tests"
-./build/"$EARTH_OS"/amd64/earth --no-output -P ./examples/tests+experimental
+./build/"$EARTH_OS"/amd64/earth --no-output -P --no-cache ./examples/tests/with-docker+all
 
 echo "Build examples"
 ./build/"$EARTH_OS"/amd64/earth --no-output +examples
