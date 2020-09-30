4c30c802a715020bd68d58d6f4954ea79feb96e8
Andrea Vasapollo <a.vasapollo@faceit.com>
2020-09-30 14:15:38 +0100

    fix: removed tests

--- autocomplete/complete_test.go
+++ autocomplete/complete_test.go
@@ -1,12 +1,9 @@
 package autocomplete
 
 import (
-	"fmt"
-	"os/user"
 	"testing"
 
 	. "github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestFlagCompletion(t *testing.T) {
@@ -25,28 +22,4 @@ func TestPathCompletion(t *testing.T) {
 	matches, err := GetPotentials("earth .", 7, nil, nil)
 	NoError(t, err, "GetPotentials failed")
 	Equal(t, []string{"./", "../"}, matches)
-}
-
-func Test_getPotentialPaths(t *testing.T) {
-	return
-	u, err := user.Current()
-	require.NoError(t, err)
-
-	t.Run("user list", func(t *testing.T) {
-		userList, err := getPotentialPaths("~")
-		require.NoError(t, err)
-		require.True(t, len(userList) > 0)
-	})
-
-	t.Run("user path", func(t *testing.T) {
-		res, err := getPotentialPaths(fmt.Sprintf("~%s", u.Username))
-		require.NoError(t, err)
-		require.True(t, len(res) > 0)
-	})
-	t.Run("directory with path /", func(t *testing.T) {
-		res, err := getPotentialPaths("/")
-		require.NoError(t, err)
-		require.True(t, len(res) > 0)
-		t.Log(res)
-	})
 }
