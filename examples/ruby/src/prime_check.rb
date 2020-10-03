4c853a0c39d0e57b42929cc578896fd43c7595d6
Gauravsaha-97 <gauravsaha385@gmail.com>
2020-10-03 20:39:08 +0530

    Added Rails example according to the issue 'Ruby on rails example #344'

---
+++ examples/ruby/src/prime_check.rb
@@ -0,0 +1,8 @@
+require 'prime' 
+
+#initialize the variable
+
+first_num = 20
+
+# check if first_num is prime or not. If prime, then 'true' is printed. Else, 'false' is printed. 
+puts first_num.prime? 
