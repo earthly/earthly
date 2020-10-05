8c74d1b417dc7a76891f69b306fdcf3e2b4b339c
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-10-05 13:00:17 -0700

    Somewhat unrelated tweaks for buildkite on windows.

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -3,7 +3,12 @@
 set -xeu
 
 echo "Add branch info back to git (Earthly uses it for tagging)"
-git checkout -b "$BUILDKITE_BRANCH" || true
+git checkout -B "$BUILDKITE_BRANCH" || true
+
+if [ "$BUILDKITE_AGENT_META_DATA_OS" == "windows" ]; then
+    # This is necessary on Windows.
+    sleep 5
+fi
 
 echo "Download latest Earthly binary"
 wget https://github.com/earthly/earthly/releases/latest/download/earth-"$EARTH_OS"-amd64 -O ./earth-released && chmod +x ./earth-released
