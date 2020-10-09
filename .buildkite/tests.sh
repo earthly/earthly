860c2935cdb3390885f7f17df054bcbc84373bf3
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-10-09 16:11:42 -0700

    Try something else.

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -5,15 +5,13 @@ set -xeu
 echo "Add branch info back to git (Earthly uses it for tagging)"
 git checkout -B "$BUILDKITE_BRANCH" || true
 
-if [ "$BUILDKITE_AGENT_META_DATA_OS" == "windows" ]; then
-    lsof ./earth-released || true
-fi
-
 echo "Download latest Earthly binary"
-curl -o ./earth-released -L https://github.com/earthly/earthly/releases/latest/download/earth-"$EARTH_OS"-amd64 && chmod +x ./earth-released
+# The use of a UUID here is necessary due to random "Text file busy" errors on Windows.
+latest_earth="./earth-released-$BUILDKITE_BUILD_ID"
+curl -o "$latest_earth" -L https://github.com/earthly/earthly/releases/latest/download/earth-"$EARTH_OS"-amd64 && chmod +x "$latest_earth"
 
 echo "Build latest earth using released earth"
-./earth-released +for-"$EARTH_OS"
+"$latest_earth" +for-"$EARTH_OS"
 
 echo "Execute tests"
 ./build/"$EARTH_OS"/amd64/earth --no-output -P +test
