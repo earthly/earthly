375bae404d6566b5c308456ccd1d5c870853891d
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-10-09 17:11:00 -0700

    We have a winner.

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -7,11 +7,16 @@ git checkout -B "$BUILDKITE_BRANCH" || true
 
 echo "Download latest Earthly binary"
 SECONDS=0
+do_reset=false
 while ! echo "." >./earth-released; do
+    do_reset=true
     echo "Waiting for ./earth-released to become available for writing..."
     echo "Time elapsed: $SECONDS seconds"
     sleep 1
 done
+if [ "$do_reset" = "true" ]; then
+    docker stop earthly-buildkitd || true
+fi
 curl -o ./earth-released -L https://github.com/earthly/earthly/releases/latest/download/earth-"$EARTH_OS"-amd64 && chmod +x ./earth-released
 
 echo "Build latest earth using released earth"
