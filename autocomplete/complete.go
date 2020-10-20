787c706f491ab0ef324dc19c368dc6b8112358e8
Alex Couture-Beil <alex@mofo.ca>
2020-10-20 16:46:16 -0700

    Fix relative autocompletion (#434)
    * Revert "feat(autocomplete): implemented functionality to autocomplete all the… (#349)"
    
    This reverts commit b77a87a60173a2c6d497ee386d3006491a7f57d3.
    
    This commit broke the ability to auto complete on earth ./somedir+...
    
    * add relative directory test
    
    * rename test targets
    
    Co-authored-by: Alex Couture-Beil <alex@earthly.dev>

--- autocomplete/complete.go
+++ autocomplete/complete.go
@@ -1,13 +1,10 @@
 package autocomplete
 
 import (
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"os/user"
 	"path"
-	"path/filepath"
 	"strings"
 
 	"github.com/earthly/earthly/earthfile2llb"
@@ -71,22 +68,14 @@ func getPotentialTarget(prefix string) ([]string, error) {
 	}
 	dirPath := splits[0]
 
+	currentUser, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+
 	realDirPath := dirPath
-	if strings.HasPrefix(realDirPath, "~") {
-		if strings.HasPrefix(realDirPath, "~/") {
-			currentUser, err := user.Current()
-			if err != nil {
-				return nil, err
-			}
-			realDirPath = strings.Replace(realDirPath, "~", currentUser.HomeDir, 1)
-		} else {
-			username := strings.Replace(strings.Split(prefix, "/")[0], "~", "", 1)
-			currentUser, err := user.Lookup(username)
-			if err != nil {
-				return nil, err
-			}
-			realDirPath = strings.Replace(realDirPath, fmt.Sprintf("~%s", username), currentUser.HomeDir, 1)
-		}
+	if strings.HasPrefix(prefix, "~/") {
+		realDirPath = currentUser.HomeDir + "/" + dirPath[2:]
 	}
 
 	targets, err := earthfile2llb.GetTargets(path.Join(realDirPath, "Earthfile"))
@@ -105,182 +94,62 @@ func getPotentialTarget(prefix string) ([]string, error) {
 	return potentials, nil
 }
 
-func getUser(username string) (*user.User, error) {
-	username = strings.Replace(username, "~", "", 1)
-	return user.Lookup(username)
-}
-
-func getUsers() ([]string, error) {
-	u, err := user.Current()
-	if err != nil {
-		return nil, err
-	}
-
-	home := filepath.Dir(u.HomeDir)
-	directoryList, err := ioutil.ReadDir(home)
-	if err != nil {
-		return nil, err
-	}
-	var res []string
-	for _, s := range directoryList {
-		if !s.IsDir() {
-			continue
-		}
-		u, err := user.Lookup(s.Name())
-		if err != nil {
-			continue
-		}
-		res = append(res, fmt.Sprintf("~%s", u.Username))
-	}
-	return res, nil
-}
-
-func matchAndFilterUser(users []string, prefix string) (match bool, filter []string) {
-	for _, s := range users {
-		if s == prefix {
-			match = true
-			return
-		}
-		if strings.HasPrefix(s, prefix) {
-			filter = append(filter, s)
-		}
-	}
-	return
-}
-
-func resolveUserPath(prefix string) ([]string, error) {
-	users, err := getUsers()
-	if err != nil {
-		return nil, err
-	}
-
-	if prefix == "~" {
-		return users, nil
-	}
-
-	replacer := ""
-	username := ""
-	if strings.HasPrefix(prefix, "~/") {
-		replacer = "~"
-		u, err := user.Current()
-		if err != nil {
-			return nil, err
-		}
-		username = fmt.Sprintf("~%s", u.Username)
-	} else {
-		// match and filter
-		sl := strings.Split(prefix, "/")
-		username = sl[0]
-		replacer = username
-		match, filters := matchAndFilterUser(users, username)
-		// filter users by prefix
-		if !match {
-			return filters, nil
-		}
-	}
-
-	u, err := getUser(username)
-	if err != nil {
-		return nil, err
-	}
-
-	if strings.HasPrefix(prefix, "~/") {
-		prefix = strings.Replace(prefix, "~", u.HomeDir, 1)
-	} else {
-		prefix = strings.Replace(prefix, username, u.HomeDir, 1)
-	}
-	var result []string
-	if hasEarthfile(prefix) {
-		x := strings.Replace(prefix, u.HomeDir, replacer, 1)
-		result = append(result, fmt.Sprintf("%s+", x))
-	}
-
-	paths, err := lsPath(prefix)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, p := range paths {
-		result = append(result, strings.Replace(p, u.HomeDir, replacer, 1))
-	}
-	return result, nil
-}
-
-func filterPath(p, filter string) ([]string, error) {
-	fi, err := os.Stat(p)
-	if err != nil {
-		return nil, err
-	}
-	if !fi.IsDir() {
-		return nil, errors.New("not a directory")
-	}
-
-	lDir, err := listDirectory(p)
-	if err != nil {
-		return nil, err
-	}
-	var res []string
-	for _, s := range lDir {
-		if strings.HasPrefix(s, path.Join(p, filter)) {
-			res = append(res, s)
-		}
-	}
-	return res, nil
-}
-
-func listDirectory(prefix string) ([]string, error) {
-	files, err := ioutil.ReadDir(prefix)
-	if err != nil {
-		return nil, err
-	}
-	var paths []string
-
-	for _, file := range files {
-		s := path.Join(prefix, file.Name())
-		if hasEarthfile(s) {
-			paths = append(paths, s+"+")
-		}
-		if hasSubDirs(s) {
-			paths = append(paths, s+"/")
-		}
-	}
-	return paths, nil
-}
-
-func lsPath(prefix string) ([]string, error) {
-	fi, err := os.Stat(prefix)
-	if err != nil {
-		d, f := path.Split(prefix)
-		return filterPath(d, f)
-	}
-
-	switch mode := fi.Mode(); {
-	case mode.IsDir():
-		return listDirectory(prefix)
-	case mode.IsRegular():
-		// do file stuff
-		return nil, errors.New("it is not a directory")
-	default:
-		return nil, errors.New("it is not a directory")
-	}
-}
-
 func getPotentialPaths(prefix string) ([]string, error) {
 	if prefix == "." {
 		return []string{"./", "../"}, nil
 	}
+	currentUser, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
 
-	// users implementation
+	// TODO expand this logic to support other users
+	if prefix == "~" {
+		prefix = "~/"
+	}
+
+	expandedHomeLen := 0
 	if strings.HasPrefix(prefix, "~") {
-		return resolveUserPath(prefix)
+		expandedHomeLen = len(currentUser.HomeDir) + 1
+		if len(prefix) > 2 {
+			prefix = currentUser.HomeDir + "/" + prefix[2:]
+		} else {
+			prefix = currentUser.HomeDir + "/"
+		}
 	}
-	// generic folder
-	var res []string
-	if hasEarthfile(prefix) {
-		res = append(res, fmt.Sprintf("%s+", prefix))
+
+	dir, f := path.Split(prefix)
+
+	replaceHomePrefix := func(s string) string {
+		if expandedHomeLen == 0 {
+			return s
+		}
+		return "~/" + s[expandedHomeLen:]
 	}
-	l, _ := lsPath(prefix)
-	return append(res, l...), nil
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	paths := []string{}
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), f) {
+			if file.IsDir() {
+				s := path.Join(dir, file.Name())
+				if strings.HasPrefix(prefix, "./") {
+					s = "./" + s
+				}
+				if hasEarthfile(s) {
+					paths = append(paths, replaceHomePrefix(s)+"+")
+				}
+				if hasSubDirs(s) {
+					paths = append(paths, replaceHomePrefix(s)+"/")
+				}
+			}
+		}
+	}
+	return paths, nil
 }
 
 // GetPotentials returns a list of potential arguments for shell auto completion
