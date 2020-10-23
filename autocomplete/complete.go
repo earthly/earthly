6020df963e86a0cdadee0320a70e5bbbe8e7535e
Alex Couture-Beil <alex@earthly.dev>
2020-10-22 17:01:35 -0700

    fix autocompletion for mac

--- autocomplete/complete.go
+++ autocomplete/complete.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	"os/user"
 	"path"
+	"path/filepath"
 	"reflect"
+	"runtime"
 	"strings"
 
 	"github.com/earthly/earthly/earthfile2llb"
@@ -69,28 +71,50 @@ func isLocalPath(path string) bool {
 
 func getUsers() (map[string]string, error) {
 	users := map[string]string{}
-	fp, err := os.Open("/etc/passwd")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to open /etc/passwd")
-	}
-	reader := bufio.NewReader(fp)
-	for {
-		line, err := reader.ReadString('\n')
+	if runtime.GOOS == "darwin" {
+		currentUser, err := user.Current()
 		if err != nil {
-			if err == io.EOF {
-				break
+			return nil, errors.Wrap(err, "failed to get current user")
+		}
+		home := filepath.Dir(currentUser.HomeDir)
+		directoryList, err := ioutil.ReadDir(home)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to read dir")
+		}
+		for _, s := range directoryList {
+			if !s.IsDir() {
+				continue
 			}
-			return nil, errors.Wrap(err, "failed to read line")
+			u, err := user.Lookup(s.Name())
+			if err != nil {
+				continue
+			}
+			users[u.Username] = path.Join(home, u.Username)
 		}
-		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line, "#") {
-			continue
+	} else {
+		fp, err := os.Open("/etc/passwd")
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to open /etc/passwd")
 		}
-		parts := strings.Split(line, ":")
-		if len(parts) >= 6 {
-			user := parts[0]
-			home := parts[5]
-			users[user] = home
+		reader := bufio.NewReader(fp)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return nil, errors.Wrap(err, "failed to read line")
+			}
+			line = strings.TrimSpace(line)
+			if strings.HasPrefix(line, "#") {
+				continue
+			}
+			parts := strings.Split(line, ":")
+			if len(parts) >= 6 {
+				user := parts[0]
+				home := parts[5]
+				users[user] = home
+			}
 		}
 	}
 	return users, nil
