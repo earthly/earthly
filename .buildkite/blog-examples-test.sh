ab9ed6a94488b75d1631c7f3a9b4ef0b176b69e8
Alex Couture-Beil <alex@earthly.dev>
2020-11-02 13:26:35 -0800

    fix blog test after master was renamed to main

--- .buildkite/blog-examples-test.sh
+++ .buildkite/blog-examples-test.sh
@@ -4,5 +4,5 @@ set -xeuo pipefail
 
 earth --version
 
-earth github.com/earthly/earthly-example-scala/simple+test
-earth github.com/earthly/earthly-example-scala/simple+docker
+earth github.com/earthly/earthly-example-scala:main/simple+test
+earth github.com/earthly/earthly-example-scala:main/simple+docker
