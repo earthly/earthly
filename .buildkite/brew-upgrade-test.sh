27ad5d4d2d0e1b71c09182de2565a70bcea006b1
Alex Couture-Beil <alex@earthly.dev>
2020-12-15 10:48:47 -0800

    Update scheduled tests to use earthly or earth (#630)
    - if earthly doesn't exist, fall back to using earth
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- .buildkite/brew-upgrade-test.sh
+++ .buildkite/brew-upgrade-test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
 set -xeuo pipefail
+
+earthly="earthly"
+if ! command -v "$earthly"; then
+    earthly="earth"
+fi
 
 brew upgrade earthly
 
-earthly --version
+$earthly --version
 
-earthly github.com/earthly/earthly/examples/go:main+docker
+$earthly github.com/earthly/earthly/examples/go:main+docker
