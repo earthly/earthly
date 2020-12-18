4ee5c4d2b6cdfd1d72ecdd5b7be851b56fb5a313
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-12-17 17:30:14 -0800

    WIP.

--- examples/tests/remote-cache/test.sh
+++ examples/tests/remote-cache/test.sh
@@ -14,23 +14,28 @@ docker network rm registry-net || true
 
 # Use a specific network so we can assign specific IP (needed for certificates).
 docker network create --subnet=172.25.0.0/16 registry-net
-registry_ip="172.25.0.1"
+export REGISTRY_IP="172.25.0.2"
+export REGISTRY="registry.local"
 
 # Generate certificates.
-"$earthly" --build-arg REGISTRY="$registry_ip:5443" +certs
+"$earthly" --build-arg REGISTRY --artifact '+certs/*' ./certs/
 
 # Run registry.
 docker run --rm -d -v "$(pwd)"/certs:/certs \
-    -p "127.0.0.1:5443:5443" \
-    --ip="$registry_ip" \
-    -e REGISTRY_HTTP_ADDR=0.0.0.0:5443 \
+    -p "127.0.0.1:5443:443" \
+    --net=registry-net \
+    --ip="$REGISTRY_IP" \
+    -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
     -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
     -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
     --name registry registry:2
 
 # Test.
 set +e
-"$earthly" --allow-privileged --build-arg REGISTRY="$registry_ip:5443" +test
+"$earthly" --allow-privileged \
+    --build-arg REGISTRY_IP \
+    --build-arg REGISTRY \
+    +test
 exit_code="$?"
 set -e
 
