4c59a380d64129d00e78bae20f4204f59b5d48c0
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-12-17 17:01:36 -0800

    WIP.

--- examples/tests/remote-cache/test.sh
+++ examples/tests/remote-cache/test.sh
@@ -3,17 +3,40 @@
 set -uxe
 set -o pipefail
 
-REGISTRY=${REGISTRY-"127.0.0.1:5000"}
+cd "$(dirname "$0")"
+
 earthly=${earthly-"../../../build/linux/amd64/earthly"}
-export EARTHLY_BUILD_ARGS="REGISTRY"
 
-docker stop regsitry || true
-docker run --rm -d -p "127.0.0.1:5000:5000" --name registry registry:latest
+# Cleanup previous run.
+docker stop registry || true
+docker rm registry || true
+docker network rm registry-net || true
 
-"$earthly" --ci +test1
-"$earthly" --ci --push +test1
+# Use a specific network so we can assign specific IP (needed for certificates).
+docker network create --subnet=172.25.0.0/16 registry-net
+registry_ip="172.25.0.1"
 
-# TODO: How to wipe cache safely.....????
+# Generate certificates.
+"$earthly" --build-arg REGISTRY="$registry_ip:5443" +certs
 
-# TODO: Do this no matter what.
-docker stop regsitry
+# Run registry.
+docker run --rm -d -v "$(pwd)"/certs:/certs \
+    -p "127.0.0.1:5443:5443" \
+    --ip="$registry_ip" \
+    -e REGISTRY_HTTP_ADDR=0.0.0.0:5443 \
+    -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+    -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+    --name registry registry:2
+
+# Test.
+set +e
+"$earthly" --allow-privileged --build-arg REGISTRY="$registry_ip:5443" +test
+exit_code="$?"
+set -e
+
+# Cleanup.
+docker stop registry
+docker network rm registry-net
+rm -rf ./certs
+
+exit "$exit_code"
