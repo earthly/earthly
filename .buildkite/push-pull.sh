03fb98e15537eba035104b1794c315211055389f
Corey Larson <corey@earthly.dev>
2021-01-19 23:28:11 -0700

    Poke

--- .buildkite/push-pull.sh
+++ .buildkite/push-pull.sh
@@ -2,7 +2,7 @@
 set -xeuo pipefail
 
 echo "Download latest Earthly binary"
-curl -o ./earthly-released -L https://github.com/earthly/earthly/releases/latest/download/earthly-"$EARTH_OS"-amd64 && chmod +x ./earthly-released
+curl -o ./earthly-released -L https://github.com/earthly/earthly/releases/latest/download/earthly-darwin-amd64 && chmod +x ./earthly-released
 
 echo "Build latest earthly using released earthly"
 ./earthly-released +for-darwin
