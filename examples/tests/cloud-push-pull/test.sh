e8dcc4c5207260e29fa0f62efa5efbe26cf7f689
Corey Larson <corey@earthly.dev>
2021-01-19 23:00:13 -0700

    Script tweaks

--- examples/tests/cloud-push-pull/test.sh
+++ examples/tests/cloud-push-pull/test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Note: Most of this test runs as Earthly-in-Earthly so that we can easily send local cache to a tmpfs.
 
-set -uxe
 set -o pipefail
 
 cd "$(dirname "$0")"
@@ -9,12 +7,7 @@ cd "$(dirname "$0")"
 earthly=${earthly-"../../../build/linux/amd64/earthly"}
 
 # Test.
-set +e
 "$earthly" --allow-privileged \
     --ci \
     +all
-exit_code="$?"
-set -e
-
-
-exit "$exit_code"
+exit "$?"
