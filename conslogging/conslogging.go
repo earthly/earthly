ae11d08a5cc3964bc94922d2af6f5382f917e150
Corey Larson <corey@earthly.dev>
2021-01-29 12:05:49 -0700

    Nit

--- conslogging/conslogging.go
+++ conslogging/conslogging.go
@@ -144,7 +144,7 @@ func (cl ConsoleLogger) PrintFailure(msg string) {
 	cl.printBar(warnColor, " FAILURE ", msg)
 }
 
-func (cl ConsoleLogger) printBar(color *color.Color, center, msg string) {
+func (cl ConsoleLogger) printBar(c *color.Color, center, msg string) {
 	if msg != "" {
 		center = fmt.Sprintf("%s[%s] ", center, msg)
 	}
@@ -159,7 +159,7 @@ func (cl ConsoleLogger) printBar(color *color.Color, center, msg string) {
 		rightBar += "="
 	}
 
-	cl.color(color).Fprintf(cl.outW, "%s%s%s\n", leftBar, center, rightBar)
+	cl.color(c).Fprintf(cl.outW, "%s%s%s\n", leftBar, center, rightBar)
 }
 
 // Warnf prints a warning message in red to errWriter
