6553834ca6485dbf97a44afef5f127d24733cb7c
Corey Larson <corey@earthly.dev>
2021-02-01 18:34:47 -0700

    CI?

---
+++ examples/tests/push-images/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ue
+set -o pipefail
+
+cd "$(dirname "$0")"
+
+earthly=${earthly-"../../../build/linux/amd64/earthly"}
+
+"$earthly" --allow-privileged --no-cache +test | tee output
+
+if ! cat output | grep "Did not push, OR save earthly/sap:after-push locally"; then
+    echo "Invalid push text"
+    cat output
+    exit 1
+fi
+
+if ! docker images --format "{{.Repository}}:{{.Tag}}" | grep "earthly/sap:after-push"; then
+    echo "Saved invalid image"
+    docker images --format "{{.Repository}}:{{.Tag}}" | grep "sap:"
+    exit 1
+fi
+
+echo "Success!"
