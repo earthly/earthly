eecf52966309edc784c7a1ca4419819c4d0da1f0
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2021-02-08 14:15:56 -0800

    Better buildkit status reporting.

--- cmd/earthly/main.go
+++ cmd/earthly/main.go
@@ -1070,7 +1070,7 @@ func (app *earthlyApp) insertZSHCompleteEntry() error {
 func (app *earthlyApp) run(ctx context.Context, args []string) int {
 	err := app.cliApp.RunContext(ctx, args)
 
-	rpcRegex := regexp.MustCompile(`(?U)rpc error: code = .+ desc = .+:\s`)
+	rpcRegex := regexp.MustCompile(`(?U)rpc error: code = .+ desc = `)
 	if err != nil {
 		if strings.Contains(err.Error(), "security.insecure is not allowed") {
 			app.console.Warnf("Error: --allow-privileged (-P) flag is required\n")
@@ -1083,8 +1083,15 @@ func (app *earthlyApp) run(ctx context.Context, args []string) int {
 		} else if !app.verbose && rpcRegex.MatchString(err.Error()) {
 			baseErr := errors.Cause(err)
 			baseErrMsg := rpcRegex.ReplaceAll([]byte(baseErr.Error()), []byte(""))
-
 			app.console.Warnf("Error: %v\n", string(baseErrMsg))
+			if bytes.Contains(baseErrMsg, []byte("transport is closing")) {
+				app.console.Warnf(
+					"It seems that buildkitd is shutting down or it has crashed. " +
+						"You can report crashes at https://github.com/earthly/earthly/issues/new. " +
+						"Buildkitd logs follow...")
+				buildkitd.PrintLogs(ctx)
+				return 7
+			}
 		} else {
 			app.console.Warnf("Error: %v\n", err)
 		}
