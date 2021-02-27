5ab77f3af7a8dbbce6da20b3878b764752747b90
Vlad A. Ionescu <446771+vladaionescu@users.noreply.github.com>
2021-02-26 17:47:53 -0800

    Allow platform contradictions - the right way this time (#826)
    * WIP.
    
    * Allow platform contradictions - the right way this time.
    
    * Minor.
    
    * Minor.
    
    * Minor.
    
    * Minor.
    
    * Minor.
    
    Co-authored-by: Vlad A. Ionescu <vladaionescu@users.noreply.github.com>

--- builder/builder.go
+++ builder/builder.go
@@ -543,10 +543,7 @@ func (b *Builder) buildOnlyLastImageAsTar(ctx context.Context, mts *states.Multi
 		return err
 	}
 
-	platform, err := llbutil.ResolvePlatform(mts.Final.Platform, opt.Platform)
-	if err != nil {
-		platform = mts.Final.Platform
-	}
+	platform := llbutil.ResolvePlatform(opt.Platform, mts.Final.Platform)
 	plat := llbutil.PlatformWithDefault(platform)
 	err = b.outputImageTar(ctx, saveImage, plat, dockerTag, outFile)
 	if err != nil {
@@ -560,14 +557,11 @@ func (b *Builder) buildMain(ctx context.Context, mts *states.MultiTarget, opt Bu
 	if b.opt.NoCache {
 		state = state.SetMarshalDefaults(llb.IgnoreCache)
 	}
-	platform, err := llbutil.ResolvePlatform(mts.Final.Platform, opt.Platform)
-	if err != nil {
-		platform = mts.Final.Platform
-	}
+	platform := llbutil.ResolvePlatform(opt.Platform, mts.Final.Platform)
 	plat := llbutil.PlatformWithDefault(platform)
-	err = b.s.solveMain(ctx, state, plat)
+	err := b.s.solveMain(ctx, state, plat)
 	if err != nil {
-		return errors.Wrapf(err, "solve side effects")
+		return err
 	}
 	return nil
 }
