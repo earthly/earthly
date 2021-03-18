39589a72e203d694f44332c963ea9ee01749373c
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2021-03-18 11:54:50 -0700

    Accept new style args on the command line too.

--- variables/parsenew_test.go
+++ variables/parsenew_test.go
@@ -21,12 +21,13 @@ func TestParseFlagArgs(t *testing.T) {
 		{[]string{"--flag\\=name=foo=bar"}, []string{"flag\\=name=foo=bar"}},
 		{[]string{"--flag=foo", "--another=bar"}, []string{"flag=foo", "another=bar"}},
 		{[]string{"--flag", "--foo", "--another", "--bar"}, []string{"flag=--foo", "another=--bar"}},
+		{[]string{"-flag", "--foo", "-another", "bar"}, []string{"flag=--foo", "another=bar"}},
 	}
 
 	for _, tt := range tests {
 		kvs, err := ParseFlagArgs(tt.kvFlag)
-		Equal(t, kvs, tt.kv)
 		NoError(t, err)
+		Equal(t, kvs, tt.kv)
 	}
 }
 
@@ -36,13 +37,36 @@ func TestNegativeParseFlagArgs(t *testing.T) {
 	}{
 		{[]string{"--foo"}},
 		{[]string{"--foo", "--bar", "--bar2"}},
-		{[]string{"-foo", "bar"}},
-		{[]string{"-foo=bar"}},
 		{[]string{"foo=bar"}},
 	}
 
 	for _, tt := range tests {
 		_, err := ParseFlagArgs(tt.kvFlag)
 		Error(t, err)
+	}
+}
+
+func TestParseFlagArgsWithNonFlags(t *testing.T) {
+	var tests = []struct {
+		kvFlag   []string
+		flags    []string
+		nonFlags []string
+	}{
+		{[]string{}, []string{}, []string{}},
+		{[]string{"--flag=foo"}, []string{"flag=foo"}, []string{}},
+		{[]string{"--flag=foo", "arg"}, []string{"flag=foo"}, []string{"arg"}},
+		{[]string{"arg", "--flag=foo"}, []string{"flag=foo"}, []string{"arg"}},
+		{[]string{"arg", "--flag=foo", "arg2", "--flag2=bar"}, []string{"flag=foo", "flag2=bar"}, []string{"arg", "arg2"}},
+		{[]string{"arg", "--flag", "foo", "arg2", "--flag2=bar"}, []string{"flag=foo", "flag2=bar"}, []string{"arg", "arg2"}},
+		{[]string{"arg", "-flag", "foo", "arg2", "-flag2=bar"}, []string{"flag=foo", "flag2=bar"}, []string{"arg", "arg2"}},
+		{[]string{"arg"}, []string{}, []string{"arg"}},
+		{[]string{"just", "args"}, []string{}, []string{"just", "args"}},
+	}
+
+	for _, tt := range tests {
+		flags, nonFlags, err := ParseFlagArgsWithNonFlags(tt.kvFlag)
+		NoError(t, err)
+		Equal(t, flags, tt.flags)
+		Equal(t, nonFlags, tt.nonFlags)
 	}
 }
