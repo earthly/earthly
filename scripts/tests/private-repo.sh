7ebf41ff5a92abaa7badc4c6c8a12adb08434ba1
Alex Couture-Beil <alex@earthly.dev>
2021-04-30 09:20:37 -0700

    test that unauthorized keys dont affect public access
    - expand test to cover case where an unauthorized key is also loaded
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- scripts/tests/private-repo.sh
+++ scripts/tests/private-repo.sh
@@ -25,6 +25,29 @@ unset EARTHLY_TOKEN
 echo starting new instance of ssh-agent, and loading credentials
 eval "$(ssh-agent)"
 
+# make sure that no keys are initially loaded
+ssh-add -l | grep 'The agent has no identities'
+
+
+# test we can still clone a public repo
+echo === Test 1 ===
+"$earthly" github.com/earthly/hello-world:main+hello
+
+
+# test we can still clone a public repo even if we have an unauthorized key-loaded
+echo === Test 2 ===
+
+# first create a new random key which is NOT authorized for any services
+ssh-keygen -b 3072 -t rsa -f /tmp/sshkey -q -N "" -C "this-key-is-not-authorized"
+ssh-add /tmp/sshkey
+
+# then test that we still have access to this public repo
+"$earthly" github.com/earthly/hello-world:main+hello
+
+
+# Test a private repo can be cloned
+echo === Test 3 ===
+
 # grab first 6chars of md5sum of key to help sanity check that the same key is consistently used
 md5sum=$(echo -n "$ID_RSA" | md5sum | awk '{ print $1 }' | head -c6)
 
@@ -34,26 +57,23 @@ echo "$ID_RSA" | ssh-add -
 echo testing that key was correctly loaded into ssh-agent
 ssh-add -l | perl -pe 'BEGIN {$status=1} END {exit $status} $status=0 if /cinnamonthecat/;'
 
-echo testing that the ssh-agent only contains a single key
-test "$(ssh-add -l | wc -l)" = "1"
-
 echo testing github connection
 ssh git@github.com 2>&1 | perl -pe 'BEGIN {$status=1} END {exit $status} $status=0 if /cinnamonthecat/;'
 
-# Test a private repo can be cloned
-echo === Test 1 ===
 docker image rm -f test-private:latest
 "$earthly" -VD github.com/cinnamonthecat/test-private:main+docker
 docker run --rm test-private:latest | grep "Salut Lume"
 
+
 # Test public repo can be cloned without ssh, when GIT_URL_INSTEAD_OF is set as recommended by our CI docs
-echo === Test 2 ===
+echo === Test 4 ===
 docker image rm -f earthly/examples:cpp
 SSH_AUTH_SOCK="" GIT_URL_INSTEAD_OF="https://github.com/=git@github.com:" $earthly -VD github.com/earthly/earthly/examples/cpp:main+docker
 docker run --rm earthly/examples:cpp | grep fib
 
+
 # Test a private repo can be cloned using https
-echo === Test 3 ===
+echo === Test 5 ===
 
 cat << EOF > /tmp/earthconfig.https
 git:
@@ -68,8 +88,9 @@ docker image rm -f other-test-private:latest
 SSH_AUTH_SOCK="" $earthly -VD --config /tmp/earthconfig.https github.com/cinnamonthecat/other-test-private:main+docker
 docker run --rm other-test-private:latest | grep "Salut le monde"
 
+
 # Test a private repo can be cloned using https, as setup via args
-echo === Test 4 ===
+echo === Test 6 ===
 
 docker image rm -f other-test-private:latest
 SSH_AUTH_SOCK="" $earthly -VD --git-username=cinnamonthecat --git-password="$GITHUB_PASSWORD" github.com/cinnamonthecat/another-test-private:main+docker
