40b69c4af48e5eb93905207284ad2074bba64e02
Corey Larson <corey@earthly.dev>
2021-05-27 09:40:07 -0600

    Command updates. Allow the client-side ones to be specified on the CLI, ones that would make BK restart in the config.

--- cmd/earthly/main.go
+++ cmd/earthly/main.go
@@ -137,6 +137,8 @@ type cliFlags struct {
 	strict                    bool
 	conversionParllelism      int
 	debuggerHost              string
+	certPath                  string
+	keyPath                   string
 }
 
 var (
@@ -424,6 +426,22 @@ func newEarthlyApp(ctx context.Context, console conslogging.ConsoleLogger) *eart
 			EnvVars:     []string{"EARTHLY_DEBUGGER_HOST"},
 			Usage:       wrap("The URL to use for connecting to a debugger host. ", "If empty, earthly uses the default debugger port, combined with the desired buildkit host."),
 			Destination: &app.debuggerHost,
+			Hidden:      true,
+		},
+		&cli.StringFlag{
+			Name:        "tlscert",
+			Value:       "./certs/earthly_cert.pem",
+			EnvVars:     []string{"EARTHLY_TLS_CERT"},
+			Usage:       wrap("The path to the client TLS cert", "If relative, will be interpreted as relative to the ~/.earthly folder."),
+			Destination: &app.certPath,
+			Hidden:      true,
+		},
+		&cli.StringFlag{
+			Name:        "tlskey",
+			Value:       "./certs/earthly_key.pem",
+			EnvVars:     []string{"EARTHLY_TLS_KEY"},
+			Usage:       wrap("The path to the client TLS key.", "If relative, will be interpreted as relative to the ~/.earthly folder."),
+			Destination: &app.keyPath,
 			Hidden:      true,
 		},
 		&cli.IntFlag{
@@ -952,6 +970,8 @@ func (app *earthlyApp) before(context *cli.Context) error {
 		if err != nil {
 			return err
 		}
+		app.handleTLSCertificateSettings(context)
+
 	case "docker-container":
 		bkAddr, dbAddr, err = app.getBuildkitAndDebuggerAddressesOriginal()
 		if err != nil {
@@ -1040,6 +1060,16 @@ func (app *earthlyApp) getBuildkitAndDebuggerAddressesForTCP(context *cli.Contex
 	}
 
 	return app.buildkitHost, app.debuggerHost, nil
+}
+
+func (app *earthlyApp) handleTLSCertificateSettings(context *cli.Context) {
+	app.buildkitdSettings.TLSCA = app.cfg.Global.TLSCA
+
+	app.buildkitdSettings.ClientTLSCert = app.certPath
+	app.buildkitdSettings.ClientTLSKey = app.keyPath
+
+	app.buildkitdSettings.ServerTLSCert = app.cfg.Global.ServerTLSCert
+	app.buildkitdSettings.ServerTLSKey = app.cfg.Global.ServerTLSKey
 }
 
 func parseAndvalidateURL(addr string) (*url.URL, error) {
@@ -1506,6 +1536,19 @@ func (app *earthlyApp) actionBootstrap(c *cli.Context) error {
 	}
 
 	if !app.bootstrapNoBuildkit {
+		if app.cfg.Global.BuildkitScheme == "tcp" {
+			root, err := cliutil.GetEarthlyDir()
+			if err != nil {
+				return err
+			}
+
+			certsDir := filepath.Join(root, "certs")
+			err = buildkitd.GenerateCertificates(certsDir)
+			if err != nil {
+				return errors.Wrap(err, "setup TLS")
+			}
+		}
+
 		// Bootstrap buildkit - pulls image and starts daemon.
 		bkClient, err := buildkitd.NewClient(c.Context, app.console, app.buildkitdImage, app.buildkitdSettings)
 		if err != nil {
