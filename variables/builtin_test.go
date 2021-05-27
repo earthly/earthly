1d1b59b1e0e2aca080b8e60a2519468cb399f40e
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2021-05-27 10:25:18 -0700

    Add a variant for the Earthly target project without the tag.

--- variables/builtin_test.go
+++ variables/builtin_test.go
@@ -20,6 +20,15 @@ func TestGetProjectName(t *testing.T) {
 		{"git@bitbucket.com:earthly/earthly", "earthly/earthly"},
 		{"ssh://git@github.com/earthly/earthly", "earthly/earthly"},
 		{"ssh://git@github.com:22/earthly/earthly", "earthly/earthly"},
+		{"http://github.com/earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"http://gitlab.com/earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"https://github.com/earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"https://user@github.com/earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"https://user:password@github.com/earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"git@github.com:earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"git@bitbucket.com:earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"ssh://git@github.com/earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
+		{"ssh://git@github.com:22/earthly/earthly/subdir/anothersubdir", "earthly/earthly/subdir/anothersubdir"},
 	}
 
 	for _, tt := range tests {
