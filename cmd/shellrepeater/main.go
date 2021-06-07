fcdcd6ec0a3ff645be3ea3cd4b208247cc7c49cb
Alex Couture-Beil <alex@earthly.dev>
2021-06-07 15:09:07 -0700

    Add debug logs to shell repeater
    This creates a logger for the shell repeater with the app label set to
    "shellrepeater" to make it easier to view shellrepeater logs vs buildkit
    logs via:
    
        docker logs earthly-buildkitd 2>&1 | grep shellrepeater
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- cmd/shellrepeater/main.go
+++ cmd/shellrepeater/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
+
 	"github.com/earthly/earthly/debugger/server"
+	"github.com/earthly/earthly/logging"
+
+	"github.com/sirupsen/logrus"
 )
 
 const addr = "0.0.0.0:8373"
 
 func main() {
-	x := server.NewServer(addr)
+	logrus.SetLevel(logrus.DebugLevel)
+	ctx := context.Background()
+	log := logging.GetLogger(ctx).With("app", "shellrepeater")
+
+	x := server.NewServer(addr, log)
 	x.Start()
 }
