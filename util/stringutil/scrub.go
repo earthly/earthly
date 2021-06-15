6c5c4add8fa0517393bcfe27d2c90f9c9a111bed
Alex Couture-Beil <alex@earthly.dev>
2021-06-15 15:15:16 -0700

    I don't want no (double) scrub
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- util/stringutil/scrub.go
+++ util/stringutil/scrub.go
@@ -1,10 +1,17 @@
 package stringutil
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 var scrubRegexp = regexp.MustCompile(`(//[^:]+):([^@]+)@`)
 
 // ScrubCredentials removes credentials from a string
 func ScrubCredentials(s string) string {
+	if strings.Contains(s, ":xxxxx@") {
+		// buildkit already does this under redact_credentials.go; don't double-scrub
+		return s
+	}
 	return string(scrubRegexp.ReplaceAll([]byte(s), []byte("$1:***@")))
 }
--- util/stringutil/scrub_test.go
+++ util/stringutil/scrub_test.go
@@ -10,3 +10,8 @@ func TestScrub(t *testing.T) {
 	s := ScrubCredentials("https://user:password@github.com/org/repo.git")
 	Equal(t, "https://user:***@github.com/org/repo.git", s)
 }
+
+func TestNoDoubleScrub(t *testing.T) {
+	s := ScrubCredentials("https://user:xxxxx@github.com/org/repo.git")
+	Equal(t, "https://user:xxxxx@github.com/org/repo.git", s)
+}
