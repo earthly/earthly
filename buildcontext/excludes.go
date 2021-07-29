78ec1eaea4ec03f7258b5ebac41e7eaadbe7ca18
Theo Andresier <49570057+Threpio@users.noreply.github.com>
2021-07-29 19:29:39 +0200

    Incoorporating .earthlyignore - Issue #1029 (#1130)
    * First run at incoorporating .earthlyignore - Sure the code could be more consise and cleaner
    
    * Updates as per comments - Using fileutil.FileExists() instead of brand new function)
    
    * Syntax cleanup and variable declaration cleanup
    
    * As per comments - Removed redundent comments and fixed import ordering/syntax

--- buildcontext/excludes.go
+++ buildcontext/excludes.go
@@ -5,11 +5,13 @@ import (
 	"path/filepath"
 
 	"github.com/docker/docker/builder/dockerignore"
+	"github.com/earthly/earthly/util/fileutil"
 	"github.com/pkg/errors"
 )
 
 // EarthIgnoreFile is the name of the earthly ignore file.
 const EarthIgnoreFile = ".earthignore"
+const EarthlyIgnoreFile = ".earthlyignore"
 
 // ImplicitExcludes is a list of implicit patterns to exclude.
 var ImplicitExcludes = []string{
@@ -17,16 +19,34 @@ var ImplicitExcludes = []string{
 	"build.earth",
 	"Earthfile",
 	EarthIgnoreFile,
+	EarthlyIgnoreFile,
 }
 
 func readExcludes(dir string) ([]string, error) {
-	filePath := filepath.Join(dir, EarthIgnoreFile)
+	var ignoreFile = EarthIgnoreFile
+
+	//EarthIgnoreFile
+	var earthIgnoreFilePath = filepath.Join(dir, EarthIgnoreFile)
+	earthExists := fileutil.FileExists(earthIgnoreFilePath)
+
+	//EarthlyIgnoreFile
+	var earthlyIgnoreFilePath = filepath.Join(dir, EarthlyIgnoreFile)
+	earthlyExists := fileutil.FileExists(earthlyIgnoreFilePath)
+
+	// Check which ones exists and which don't
+	if earthExists && earthlyExists {
+		// if both exist then throw an error
+		return ImplicitExcludes, errors.New("both .earthignore and .earthlyignore exist - please remove one")
+	} else if earthExists == earthlyExists {
+		// return just ImplicitExcludes if neither of them exist
+		return ImplicitExcludes, nil
+	} else if earthlyExists {
+		ignoreFile = EarthlyIgnoreFile
+	}
+
+	filePath := filepath.Join(dir, ignoreFile)
 	f, err := os.Open(filePath)
 	if err != nil {
-		if os.IsNotExist(err) {
-			// No earthignore file present.
-			return ImplicitExcludes, nil
-		}
 		return nil, errors.Wrapf(err, "read %s", filePath)
 	}
 	excludes, err := dockerignore.ReadAll(f)
