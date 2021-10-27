e96a00d4daa60c5308a675c51f29868b3571ec5e
Andrew Sy Kim <andrew@earthly.dev>
2021-10-27 16:30:23 -0400

    add check for gRPC context cancelled error
    Signed-off-by: Andrew Sy Kim <andrew@earthly.dev>

--- cmd/earthly/main.go
+++ cmd/earthly/main.go
@@ -49,6 +49,8 @@ import (
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
 	"golang.org/x/term"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/earthly/earthly/analytics"
 	"github.com/earthly/earthly/ast"
@@ -1552,6 +1554,11 @@ func (app *earthlyApp) run(ctx context.Context, args []string) int {
 			app.console.Warnf("Error: %v\n", err)
 		}
 		if errors.Is(err, context.Canceled) {
+			app.console.Warnf("Context cancelled: %v\n", err)
+			return 2
+		}
+		if status.Code(err) == codes.Canceled {
+			app.console.Warnf("Context cancelled from buildkitd: %v\n", err)
 			return 2
 		}
 		return 1
