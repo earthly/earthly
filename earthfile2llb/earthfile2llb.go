f8a5d146547728d5b9474e8d30426eacb2c8701c
Alex Couture-Beil <alex@earthly.dev>
2021-12-16 15:15:26 -0800

    fix autocompletion of ARGs containing flags
    autocompletion for ARGS with flags (e.g. ARG --required FOO)
    was incorrectly suggesting
    
        +target ----required
    
    rather than
    
        +target --FOO`.
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- earthfile2llb/earthfile2llb.go
+++ earthfile2llb/earthfile2llb.go
@@ -191,8 +191,12 @@ func GetTargetArgs(ctx context.Context, resolver *buildcontext.Resolver, gwClien
 	}
 	var args []string
 	for _, stmt := range t.Recipe {
-		if stmt.Command.Name == "ARG" {
-			args = append(args, stmt.Command.Args[0])
+		if stmt.Command != nil && stmt.Command.Name == "ARG" {
+			_, argName, _, err := parseArgArgs(ctx, *stmt.Command)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to parse ARG arguments %v", stmt.Command.Args)
+			}
+			args = append(args, argName)
 		}
 	}
 	return args, nil
