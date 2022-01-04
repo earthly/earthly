be6d8b1a5cec098186649a5ecad9d2d24828ec85
Alex Couture-Beil <alex@earthly.dev>
2022-01-04 12:08:09 -0800

    test against current branch rather than main (#1536)
    Currently if main is broken, and fixed in a PR,
    this test will fail until main is fixed. This makes it impossible
    to submit a PR and validate the PR fixes main until after it gets
    merged to main.
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- scripts/tests/auth/test-with-git-url-instead-of.sh
+++ scripts/tests/auth/test-with-git-url-instead-of.sh
@@ -3,6 +3,9 @@ set -eu # don't use -x as it will leak the private key
 # shellcheck source=./setup.sh
 source "$(dirname "$0")/setup.sh"
 
+gitsha=$(git rev-parse HEAD)
+test -n "$gitsha"
+
 docker image rm -f earthly/examples:cpp
-GIT_URL_INSTEAD_OF="https://github.com/=git@github.com:" $earthly -VD github.com/earthly/earthly/examples/cpp:main+docker
+GIT_URL_INSTEAD_OF="https://github.com/=git@github.com:" "$earthly" -VD "github.com/earthly/earthly/examples/cpp:$gitsha+docker"
 docker run --rm earthly/examples:cpp | grep fib
