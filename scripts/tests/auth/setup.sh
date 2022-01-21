209c57e03236e079e232f30fdcf1eb695cd3ef23
Alex Couture-Beil <alex@earthly.dev>
2022-01-20 16:06:49 -0800

    test cleanup
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- scripts/tests/auth/setup.sh
+++ scripts/tests/auth/setup.sh
@@ -13,12 +13,7 @@ echo "running tests with $earthly"
 # ensure earthly login works (and print out who gets logged in)
 "$earthly" account login
 
-# fetch shared secret key (this step assumes your personal user has access to the /earthly-technologies/ secrets org
-ID_RSA=$($earthly secrets get -n /earthly-technologies/github/other-service+github-cinnamon@earthly.dev/id_rsa)
-GITHUB_PASSWORD=$($earthly secrets get -n /earthly-technologies/github/other-service+github-cinnamon@earthly.dev/password)
-REPO_ACCESS_TOKEN=$($earthly secrets get -n /earthly-technologies/github/other-service+github-cinnamon@earthly.dev/repo-access-token)
-
-# now that we grabbed the cinnamonthecat credentials, unset our token, to ensure that we're only testing using cinnamonthecat's credentials
+# these tests require the EARTHLY_TOKEN not be set
 unset EARTHLY_TOKEN
 
 # make sure ssh-agent is not running
