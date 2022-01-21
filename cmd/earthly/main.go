earthly/earthly

commit c056afc75f6daeb25cd1f7144acaa958dfcbbea4

--env-file arg to support different .env file (#1593)
Allow a user to specify an alternative `.env` file to source
environment variables from.

A user may load multiple `.env` files using process
substitution. For example, if a user has `.env` committed to git
containing:

    PUBLIC_VALUE=this-is-public-information
    MY_TOKEN=

And a second `.env.local` which is not committed to git:

    MY_TOKEN=secretvalue

A user can then have earthly combine these two files using:

    earthly --env-file <(cat .env && cat .env.local) +some-target

When multiple values are defined, the last one takes precedence.

cmd/earthly/main.go
@@ -7,6 +7,7 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"net"
@@ -84,9 +85,10 @@ const (
 	DefaultBuildkitdContainerName = "earthly-buildkitd"
 	// DefaultBuildkitdVolumeName is the name of the docker volume used for storing the cache.
 	DefaultBuildkitdVolumeName = "earthly-cache"
-)
 
-var dotEnvPath = ".env"
+	defaultEnvFile = ".env"
+	envFileFlag    = "env-file"
+)
 
 type earthlyApp struct {
 	cliApp      *cli.App
@@ -159,6 +161,7 @@ type cliFlags struct {
 	disableAnalytics          bool
 	featureFlagOverrides      string
 	localRegistryHost         string
+	envFile                   string
 	lsShowLong                bool
 	lsShowArgs                bool
 	containerFrontend         containerutil.ContainerFrontend
@@ -215,10 +218,33 @@ func main() {
 
 	// Load .env into current global env's. This is mainly for applying Earthly settings.
 	// Separate call is made for build args and secrets.
-	if fileutil.FileExists(dotEnvPath) {
-		err := godotenv.Load(dotEnvPath)
-		if err != nil {
-			fmt.Printf("Error loading dot-env file %s: %s\n", dotEnvPath, err.Error())
+	envFile := defaultEnvFile
+	envFileOverride := false
+	if envFileFromEnv, ok := os.LookupEnv("EARTHLY_ENV_FILE"); ok {
+		envFile = envFileFromEnv
+		envFileOverride = true
+	}
+	envFileFromArgOK := true
+	flagSet := flag.NewFlagSet(getBinaryName(), flag.ContinueOnError)
+	for _, f := range newEarthlyApp(ctx, conslogging.ConsoleLogger{}).cliApp.Flags {
+		if err := f.Apply(flagSet); err != nil {
+			envFileFromArgOK = false
+			break
+		}
+	}
+	if envFileFromArgOK {
+		if err := flagSet.Parse(os.Args[1:]); err == nil {
+			if envFileFlag := flagSet.Lookup(envFileFlag); envFileFlag != nil {
+				envFile = envFileFlag.Value.String()
+				envFileOverride = envFile != defaultEnvFile // flag lib doesn't expose if a value was set or not
+			}
+		}
+	}
+	err := godotenv.Load(envFile)
+	if err != nil {
+		// ignore ErrNotExist when using default .env file
+		if envFileOverride || !errors.Is(err, os.ErrNotExist) {
+			fmt.Printf("Error loading dot-env file %s: %s\n", envFile, err.Error())
 			os.Exit(1)
 		}
 	}
@@ -590,6 +616,13 @@ func newEarthlyApp(ctx context.Context, console conslogging.ConsoleLogger) *eart
 			Usage:       "Apply additional flags after each VERSION command across all Earthfiles, multiple flags can be seperated by commas",
 			Destination: &app.featureFlagOverrides,
 			Hidden:      true, // used for feature-flipping from ./earthly dev script
+		},
+		&cli.StringFlag{
+			Name:        envFileFlag,
+			EnvVars:     []string{"EARTHLY_ENV_FILE"},
+			Usage:       "Use values from this file as earthly environment variables, buildargs, or secrets",
+			Value:       defaultEnvFile,
+			Destination: &app.envFile,
 		},
 	}
 
@@ -2777,13 +2810,14 @@ func (app *earthlyApp) actionBuildImp(c *cli.Context, flagArgs, nonFlagArgs []st
 		platformsSlice = []*specs.Platform{nil}
 	}
 
-	dotEnvMap := make(map[string]string)
-	if fileutil.FileExists(dotEnvPath) {
-		dotEnvMap, err = godotenv.Read(dotEnvPath)
-		if err != nil {
-			return errors.Wrapf(err, "read %s", dotEnvPath)
+	dotEnvMap, err := godotenv.Read(app.envFile)
+	if err != nil {
+		// ignore ErrNotExist when using default .env file
+		if app.envFile != defaultEnvFile || !errors.Is(err, os.ErrNotExist) {
+			return errors.Wrapf(err, "read %s", app.envFile)
 		}
 	}
+
 	secretsMap, err := processSecrets(app.secrets.Value(), app.secretFiles.Value(), dotEnvMap)
 	if err != nil {
 		return err

tests/Earthfile
@@ -452,8 +452,16 @@ dotenv-test:
     RUN echo "TEST_ENV_3=bar" >>.env
     DO +RUN_EARTHLY --earthfile=dotenv.earth --extra_args="--no-output" --target=+test
     # Smoke test that no .env file does not result in an error.
-    RUN rm .env
+    RUN mv .env .some-other-env
     DO +RUN_EARTHLY --earthfile=dotenv.earth --extra_args="--no-output" --target=+test-no-dotenv
+    DO +RUN_EARTHLY --earthfile=dotenv.earth --extra_args="--no-output --env-file .some-other-env" --target=+test
+    DO +RUN_EARTHLY --earthfile=dotenv.earth --extra_args="--no-output" --pre_command="export EARTHLY_ENV_FILE=.some-other-env" --target=+test
+    DO +RUN_EARTHLY --earthfile=dotenv.earth --extra_args="--no-output --env-file .this-should-fail" --should_fail="true" --target=+test
+    DO +RUN_EARTHLY --earthfile=dotenv.earth --extra_args="--no-output" --pre_command="export EARTHLY_ENV_FILE=.this-too-should-fail" --should_fail="true" --target=+test
+
+    # --env-file takes preceddence
+    DO +RUN_EARTHLY --earthfile=dotenv.earth --extra_args="--no-output --env-file .some-other-env" --pre_command="export EARTHLY_ENV_FILE=.this-should-be-ignored" --target=+test
+
 
 env-test:
     DO +RUN_EARTHLY --earthfile=env.earth --extra_args="--no-output" --target=+test

commit 2d41b3a189ebf8546cce85cb0156e8e863f7580d

fix --version and --help text output
This fixes a bug that was introduced in c056afc75f6daeb25cd1f7144acaa958dfcbbea4
which caused help text to be output when --version was run, and
duplicated help text for --help.

Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

cmd/earthly/main.go
@@ -226,6 +226,7 @@ func main() {
 	}
 	envFileFromArgOK := true
 	flagSet := flag.NewFlagSet(getBinaryName(), flag.ContinueOnError)
+	flagSet.SetOutput(io.Discard)
 	for _, f := range newEarthlyApp(ctx, conslogging.ConsoleLogger{}).cliApp.Flags {
 		if err := f.Apply(flagSet); err != nil {
 			envFileFromArgOK = false

tests/Earthfile
@@ -144,7 +144,7 @@ ga:
     BUILD +true-false-flag-invalid
     BUILD +dont-save-indirect-remote-artifact
     BUILD +sequential-locally-test
-    BUILD +homebrew-test
+    BUILD +version-flag-test
     BUILD +implicit-ignores
     BUILD +help
     BUILD +cache-cmd
@@ -906,9 +906,11 @@ echo test passed: RUNs were sequentially grouped
 " > test-output.sh && chmod +x test-output.sh
     RUN cat output-filtered.txt | ./test-output.sh
 
-homebrew-test:
-    # This is to ensure the assert in https://github.com/earthly/homebrew-earthly/blob/main/Formula/earthly.rb continues to work
+version-flag-test:
+    # In addition to testing the --version flag works; this flag is critical for the homebrew deployment test, which
+    # performs an assert in https://github.com/earthly/homebrew-earthly/blob/main/Formula/earthly.rb
     RUN earthly --version | grep '^earthly version'
+    RUN test "$(earthly --version | wc -l)" = "1"
 
 implicit-ignores:
     RUN mkdir -p ignored notignored && \