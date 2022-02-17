8926986aeb5b7b2368111343223a9f6dbce1dbb0
Brandon Schurman <brandon@earthly.dev>
2022-02-17 17:17:04 -0500

    windows path when running earthly commands

--- domain/domain_windows_test.go
+++ domain/domain_windows_test.go
@@ -1,9 +1,10 @@
-//go:build !windows
-// +build !windows
+//go:build windows
+// +build windows
 
 package domain
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/stretchr/testify/assert"
@@ -15,17 +16,13 @@ var targetTests = []struct {
 }{
 	{"+target", Target{Target: "target", LocalPath: "."}},
 	{"+another-target", Target{Target: "another-target", LocalPath: "."}},
-	{"./a/local/dir+target", Target{Target: "target", LocalPath: "./a/local/dir"}},
-	{"/abs/local/dir+target", Target{Target: "target", LocalPath: "/abs/local/dir"}},
-	{"../rel/local/dir+target", Target{Target: "target", LocalPath: "../rel/local/dir"}},
+	{`.\rel\win\dir+target`, Target{Target: "target", LocalPath: `.\rel\win\dir`}},
+	{`./rel/win/dir+target`, Target{Target: "target", LocalPath: `./rel/win/dir`}},
+	{`C:\abs\win\dir+target`, Target{Target: "target", LocalPath: `C:\abs\win\dir`}},
 	{"github.com/foo/bar+target", Target{Target: "target", GitURL: "github.com/foo/bar"}},
 	{"github.com/foo/bar:tag+target", Target{Target: "target", GitURL: "github.com/foo/bar", Tag: "tag"}},
 	{"github.com/foo/bar:tag/with/slash+target", Target{Target: "target", GitURL: "github.com/foo/bar", Tag: "tag/with/slash"}},
 	{"import+target", Target{Target: "target", ImportRef: "import"}},
-	// "\+"
-	{"./a/local/dir-with-\\+-in-it+target", Target{Target: "target", LocalPath: "./a/local/dir-with-+-in-it"}},
-	{"/abs/local/dir-with-\\+-in+target", Target{Target: "target", LocalPath: "/abs/local/dir-with-+-in"}},
-	{"../rel/local/dir-with-\\+-in+target", Target{Target: "target", LocalPath: "../rel/local/dir-with-+-in"}},
 	{"github.com/foo/bar/dir-with-\\+-in+target", Target{Target: "target", GitURL: "github.com/foo/bar/dir-with-+-in"}},
 	{"github.com/foo/bar:tag-with-\\+-in+target", Target{Target: "target", GitURL: "github.com/foo/bar", Tag: "tag-with-+-in"}},
 }
@@ -34,7 +31,7 @@ var targetNegativeTests = []string{
 	"+COMMAND", "./something+COMMAND", "nope", "abc+cde+efg", "+target/artifact",
 }
 
-func TestTargetParser(t *testing.T) {
+func TestTargetParserWin(t *testing.T) {
 	for _, tt := range targetTests {
 		t.Run(tt.in, func(t *testing.T) {
 			out, err := ParseTarget(tt.in)
@@ -44,7 +41,7 @@ func TestTargetParser(t *testing.T) {
 	}
 }
 
-func TestTargetParserNegative(t *testing.T) {
+func TestTargetParserNegativeWin(t *testing.T) {
 	for _, tt := range targetNegativeTests {
 		t.Run(tt, func(t *testing.T) {
 			_, err := ParseTarget(tt)
@@ -53,7 +50,7 @@ func TestTargetParserNegative(t *testing.T) {
 	}
 }
 
-func TestTargetToString(t *testing.T) {
+func TestTargetToStringWin(t *testing.T) {
 	for _, tt := range targetTests {
 		t.Run(tt.in, func(t *testing.T) {
 			str := tt.out.String()
@@ -70,25 +67,14 @@ var artifactTests = []struct {
 	{"+another-target/another-artifact", Artifact{Target: Target{Target: "another-target", LocalPath: "."}, Artifact: "/another-artifact"}},
 	{"+another-target/deep/artifact", Artifact{Target: Target{Target: "another-target", LocalPath: "."}, Artifact: "/deep/artifact"}},
 	{"+another-target/deep/artifact/with/*", Artifact{Target: Target{Target: "another-target", LocalPath: "."}, Artifact: "/deep/artifact/with/*"}},
-	{"./a/local/dir+target/artifact", Artifact{Target: Target{Target: "target", LocalPath: "./a/local/dir"}, Artifact: "/artifact"}},
+	{`.\rel\win\dir+target/artifact`, Artifact{Target: Target{Target: "target", LocalPath: `.\rel\win\dir`}, Artifact: "/artifact"}},
+	{`./rel/win/dir+target/artifact`, Artifact{Target: Target{Target: "target", LocalPath: `./rel/win/dir`}, Artifact: "/artifact"}},
+	{`C:\abs\win\dir+target/artifact`, Artifact{Target: Target{Target: "target", LocalPath: `C:\abs\win\dir`}, Artifact: "/artifact"}},
 	{"github.com/foo/bar+target/artifact", Artifact{Target: Target{Target: "target", GitURL: "github.com/foo/bar"}, Artifact: "/artifact"}},
 	{"github.com/foo/bar:tag+target/artifact", Artifact{Target: Target{Target: "target", GitURL: "github.com/foo/bar", Tag: "tag"}, Artifact: "/artifact"}},
 	{"github.com/foo/bar:tag/with/slash+target/artifact", Artifact{Target: Target{Target: "target", GitURL: "github.com/foo/bar", Tag: "tag/with/slash"}, Artifact: "/artifact"}},
-	{"import+target/artifact", Artifact{Target: Target{Target: "target", ImportRef: "import"}, Artifact: "/artifact"}},
-	// \+ in target
-	{"./a/local/dir-with-\\+-in-it+target/artifact", Artifact{Target: Target{Target: "target", LocalPath: "./a/local/dir-with-+-in-it"}, Artifact: "/artifact"}},
-	{"/abs/local/dir-with-\\+-in+target/artifact", Artifact{Target: Target{Target: "target", LocalPath: "/abs/local/dir-with-+-in"}, Artifact: "/artifact"}},
-	{"../rel/local/dir-with-\\+-in+target/artifact", Artifact{Target: Target{Target: "target", LocalPath: "../rel/local/dir-with-+-in"}, Artifact: "/artifact"}},
 	{"github.com/foo/bar/dir-with-\\+-in+target/artifact", Artifact{Target: Target{Target: "target", GitURL: "github.com/foo/bar/dir-with-+-in"}, Artifact: "/artifact"}},
 	{"github.com/foo/bar:tag-with-\\+-in+target/artifact", Artifact{Target: Target{Target: "target", GitURL: "github.com/foo/bar", Tag: "tag-with-+-in"}, Artifact: "/artifact"}},
-	// \+ in artifact
-	{"+target/artifact-with-\\+", Artifact{Target: Target{Target: "target", LocalPath: "."}, Artifact: "/artifact-with-+"}},
-	{"+another-target/deep/artifact-with-\\+/in/it", Artifact{Target: Target{Target: "another-target", LocalPath: "."}, Artifact: "/deep/artifact-with-+/in/it"}},
-	{"+another-target/deep/artifact/with-\\+/and/*", Artifact{Target: Target{Target: "another-target", LocalPath: "."}, Artifact: "/deep/artifact/with-+/and/*"}},
-	// \+ in target and artifact
-	{"./a/local/dir-with-\\+-in-it+target/artifact-with-\\+/in/it", Artifact{Target: Target{Target: "target", LocalPath: "./a/local/dir-with-+-in-it"}, Artifact: "/artifact-with-+/in/it"}},
-	{"/abs/local/dir-with-\\+-in+target/artifact-with-\\+/in/it", Artifact{Target: Target{Target: "target", LocalPath: "/abs/local/dir-with-+-in"}, Artifact: "/artifact-with-+/in/it"}},
-	{"../rel/local/dir-with-\\+-in+target/artifact-with-\\+/in/it", Artifact{Target: Target{Target: "target", LocalPath: "../rel/local/dir-with-+-in"}, Artifact: "/artifact-with-+/in/it"}},
 	{"github.com/foo/bar/dir-with-\\+-in+target/artifact-with-\\+/in/it", Artifact{Target: Target{Target: "target", GitURL: "github.com/foo/bar/dir-with-+-in"}, Artifact: "/artifact-with-+/in/it"}},
 	{"github.com/foo/bar:tag-with-\\+-in+target/artifact-with-\\+/in/it", Artifact{Target: Target{Target: "target", GitURL: "github.com/foo/bar", Tag: "tag-with-+-in"}, Artifact: "/artifact-with-+/in/it"}},
 }
@@ -97,7 +83,8 @@ var artifactNegativeTests = []string{
 	"+COMMAND/art", "./something+COMMAND/art", "nope/art", "abc+cde+efg/art", "+just-target",
 }
 
-func TestArtifactParser(t *testing.T) {
+func TestArtifactParserWin(t *testing.T) {
+	fmt.Println("WINDOWS!")
 	for _, tt := range artifactTests {
 		t.Run(tt.in, func(t *testing.T) {
 			out, err := ParseArtifact(tt.in)
@@ -107,7 +94,7 @@ func TestArtifactParser(t *testing.T) {
 	}
 }
 
-func TestArtifactParserNegative(t *testing.T) {
+func TestArtifactParserNegativeWin(t *testing.T) {
 	for _, tt := range artifactNegativeTests {
 		t.Run(tt, func(t *testing.T) {
 			_, err := ParseArtifact(tt)
@@ -116,7 +103,7 @@ func TestArtifactParserNegative(t *testing.T) {
 	}
 }
 
-func TestArtifactToString(t *testing.T) {
+func TestArtifactToStringWin(t *testing.T) {
 	for _, tt := range artifactTests {
 		t.Run(tt.in, func(t *testing.T) {
 			str := tt.out.String()
@@ -131,17 +118,13 @@ var commandTests = []struct {
 }{
 	{"+COMMAND", Command{Command: "COMMAND", LocalPath: "."}},
 	{"+ANOTHER_COMMAND", Command{Command: "ANOTHER_COMMAND", LocalPath: "."}},
-	{"./a/local/dir+COMMAND", Command{Command: "COMMAND", LocalPath: "./a/local/dir"}},
-	{"/abs/local/dir+COMMAND", Command{Command: "COMMAND", LocalPath: "/abs/local/dir"}},
-	{"../rel/local/dir+COMMAND", Command{Command: "COMMAND", LocalPath: "../rel/local/dir"}},
+	{`.\rel\win\dir+COMMAND`, Command{Command: "COMMAND", LocalPath: `.\rel\win\dir`}},
+	{`./rel/win/dir+COMMAND`, Command{Command: "COMMAND", LocalPath: `./rel/win/dir`}},
+	{`C:\abs\win\dir+COMMAND`, Command{Command: "COMMAND", LocalPath: `C:\abs\win\dir`}},
 	{"github.com/foo/bar+COMMAND", Command{Command: "COMMAND", GitURL: "github.com/foo/bar"}},
 	{"github.com/foo/bar:tag+COMMAND", Command{Command: "COMMAND", GitURL: "github.com/foo/bar", Tag: "tag"}},
 	{"github.com/foo/bar:tag/with/slash+COMMAND", Command{Command: "COMMAND", GitURL: "github.com/foo/bar", Tag: "tag/with/slash"}},
 	{"import+COMMAND", Command{Command: "COMMAND", ImportRef: "import"}},
-	// \+
-	{"./a/local/dir-with-\\+-in-it+COMMAND", Command{Command: "COMMAND", LocalPath: "./a/local/dir-with-+-in-it"}},
-	{"/abs/local/dir-with-\\+-in+COMMAND", Command{Command: "COMMAND", LocalPath: "/abs/local/dir-with-+-in"}},
-	{"../rel/local/dir-with-\\+-in+COMMAND", Command{Command: "COMMAND", LocalPath: "../rel/local/dir-with-+-in"}},
 	{"github.com/foo/bar/dir-with-\\+-in+COMMAND", Command{Command: "COMMAND", GitURL: "github.com/foo/bar/dir-with-+-in"}},
 	{"github.com/foo/bar:tag-with-\\+-in+COMMAND", Command{Command: "COMMAND", GitURL: "github.com/foo/bar", Tag: "tag-with-+-in"}},
 }
@@ -150,7 +133,7 @@ var commandNegativeTests = []string{
 	"+target", "./something+target", "nope", "NOPE", "ABC+DEF+EFG", "+COMMAND/artifact",
 }
 
-func TestCommandParser(t *testing.T) {
+func TestCommandParserWin(t *testing.T) {
 	for _, tt := range commandTests {
 		t.Run(tt.in, func(t *testing.T) {
 			out, err := ParseCommand(tt.in)
@@ -160,7 +143,7 @@ func TestCommandParser(t *testing.T) {
 	}
 }
 
-func TestCommandParserNegative(t *testing.T) {
+func TestCommandParserNegativeWin(t *testing.T) {
 	for _, tt := range commandNegativeTests {
 		t.Run(tt, func(t *testing.T) {
 			_, err := ParseCommand(tt)
@@ -169,7 +152,7 @@ func TestCommandParserNegative(t *testing.T) {
 	}
 }
 
-func TestCommandToString(t *testing.T) {
+func TestCommandToStringWin(t *testing.T) {
 	for _, tt := range commandTests {
 		t.Run(tt.in, func(t *testing.T) {
 			str := tt.out.String()
