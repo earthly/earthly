73106200a18cecafa14354d3d94f24413c00aa35
Roberto Villarreal <rrjjvv@yahoo.com>
2022-03-10 18:17:14 -0700

    Refactor calculation of effective cache value
    This simplifies the logic and messaging, though slightly more LOC.
    
    One downside is that it no longer explicitly tells you the comparison being made, but anybody looking at this output would likely know they can easily run with `--debug` to see those values.

--- buildkitd/entrypoint.sh
+++ buildkitd/entrypoint.sh
@@ -138,8 +138,13 @@ envsubst </etc/cni/cni-conf.json.template >/etc/cni/cni-conf.json
 export BUILDKIT_ROOT_DIR="$EARTHLY_TMP_DIR"/buildkit
 mkdir -p "$BUILDKIT_ROOT_DIR"
 CACHE_SETTINGS=
+
+# For clarity; this will be become CACHE_SIZE_MB after everything is calculated.  It is intentionally left unset
+# (and not "0") to simplify the logic.
+EFFECTIVE_CACHE_SIZE_MB=
+
 if [ "$CACHE_SIZE_MB" -gt "0" ]; then
-    CACHE_SETTINGS="$(envsubst </etc/buildkitd.cache.template)"
+    EFFECTIVE_CACHE_SIZE_MB="$CACHE_SIZE_MB"
 fi
 
 if [ "$CACHE_SIZE_PCT" -gt "0" ]; then
@@ -147,18 +152,24 @@ if [ "$CACHE_SIZE_PCT" -gt "0" ]; then
     # %S -> "Fundamental block size"
     # -f $EARTHLY_TMP_DIR -> filesystem where directory resides, usually a volume in docker's root directory
     CALCULATED_CACHE_MB="$(stat -c "%b * %S * ${CACHE_SIZE_PCT} / 100 / 1024 / 1024" -f "$EARTHLY_TMP_DIR" | bc)"
-    echo "cache size set to ${CALCULATED_CACHE_MB} MB (${CACHE_SIZE_PCT}% of filesystem)"
-
-    if [ "$CACHE_SIZE_MB" -gt "0" ]; then
-        echo "Lesser of specified size ($CACHE_SIZE_MB) and percentage ($CALCULATED_CACHE_MB) will be used"
-        if [ "$CALCULATED_CACHE_MB" -lt "$CACHE_SIZE_MB" ]; then
-            CACHE_SIZE_MB="$CALCULATED_CACHE_MB"
-            CACHE_SETTINGS="$(envsubst </etc/buildkitd.cache.template)"
-        fi
+    if [ -z "$EFFECTIVE_CACHE_SIZE_MB" ]; then
+        EFFECTIVE_CACHE_SIZE_MB="$CALCULATED_CACHE_MB"
+    elif [ "$CALCULATED_CACHE_MB" -lt "$EFFECTIVE_CACHE_SIZE_MB" ]; then
+        echo "clamping cache size to $CALCULATED_CACHE_MB MB (${CACHE_SIZE_PCT}% of filesystem)"
+        EFFECTIVE_CACHE_SIZE_MB="$CALCULATED_CACHE_MB"
     else
-        CACHE_SIZE_MB="$CALCULATED_CACHE_MB"
-        CACHE_SETTINGS="$(envsubst </etc/buildkitd.cache.template)"
+        # In the off-chance they are actual equal, I'm not sure there's much value in calling that out specifically.
+        # Even if they are both "30GB", the user likely set "30000", whereas the percentage would likely come out to
+        # be something like "30314" (since we're moving from bytes, unlikely to have a consecutive set of zeroes).
+        echo "clamping cache size to fixed size of $EFFECTIVE_CACHE_SIZE_MB MB"
     fi
+fi
+
+# Remains unset if neither percent nor size were specified.  It would be simpler to just process whether it was
+# set (or not), but we'll continue setting to "0" in case anyone has become dependent on that behavior.
+CACHE_SIZE_MB="${EFFECTIVE_CACHE_SIZE_MB:-0}"
+if [ "$CACHE_SIZE_MB" -gt "0" ]; then
+    CACHE_SETTINGS="$(envsubst </etc/buildkitd.cache.template)"
 fi
 export CACHE_SETTINGS
 
