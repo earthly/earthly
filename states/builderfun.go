a4d1ca03820ea8d7148b9125cef7c911d216eef9
Mike <mikejholly@gmail.com>
2022-05-09 10:56:05 -0700

    Removed unused interface.

--- states/builderfun.go
+++ states/builderfun.go
@@ -12,21 +12,6 @@ type DockerTarImageSolver interface {
 	SolveImage(ctx context.Context, mts *MultiTarget, dockerTag string, outFile string, printOutput bool) error
 }
 
-// ImageSolverResult contains data and channels that allow one to act on images
-// during and after they are built.
-type ImageSolverResult struct {
-	ImageName   string
-	ResultChan  chan string
-	ErrChan     chan error
-	ReleaseFunc func()
-}
-
-// DockerImageSolver can create a Docker image for the WITH DOCKER command using
-// the embedded BuildKit registry.
-type DockerImageSolver interface {
-	SolveImage(ctx context.Context, mts *MultiTarget, dockerTag string) (*ImageSolverResult, error)
-}
-
 // ImageSolverResults contains data and channels that allow one to act on images
 // during and after they are built.
 type ImageSolverResults struct {
