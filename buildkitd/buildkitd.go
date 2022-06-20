1b15eb418c1fa92287df2d8261032d1c18bc22bc
Brandon Schurman <brandon@earthly.dev>
2022-06-20 11:59:49 -0400

    remove println

--- buildkitd/buildkitd.go
+++ buildkitd/buildkitd.go
@@ -561,7 +561,6 @@ func checkConnection(ctx context.Context, address string, opts ...client.ClientO
 		if err != nil {
 			connErrMu.Lock()
 			connErr = err
-			fmt.Println(err.Error())
 			connErrMu.Unlock()
 			return
 		}
