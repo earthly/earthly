7671cd029174a40d43d976d61043ef5053a27d63
Keisuke Emi <keisuke-emi@cybozu.co.jp>
2022-08-03 08:30:30 +0900

    Add `Has()` method into `CacheImports`

--- states/cacheimports.go
+++ states/cacheimports.go
@@ -5,7 +5,8 @@ import "sync"
 // CacheImports is a synchronized set of cache imports.
 type CacheImports struct {
 	mu    sync.RWMutex
-	store []string
+	slice []string
+	store map[string]bool
 }
 
 // NewCacheImports creates a new cache imports structure.
@@ -13,8 +14,14 @@ func NewCacheImports(imports []string) *CacheImports {
 	clone := make([]string, len(imports))
 	copy(clone, imports)
 
+	store := make(map[string]bool)
+	for _, tag := range imports {
+		store[tag] = true
+	}
+
 	return &CacheImports{
-		store: clone,
+		slice: clone,
+		store: store,
 	}
 }
 
@@ -23,7 +30,21 @@ func (ci *CacheImports) Add(tags ...string) {
 	ci.mu.Lock()
 	defer ci.mu.Unlock()
 
-	ci.store = append(ci.store, tags...)
+	ci.slice = append(ci.slice, tags...)
+
+	for _, tag := range tags {
+		ci.store[tag] = true
+	}
+}
+
+// Has checks if a passed tag is added.
+func (ci *CacheImports) Has(tag string) bool {
+	ci.mu.RLock()
+	defer ci.mu.RUnlock()
+
+	_, ok := ci.store[tag]
+
+	return ok
 }
 
 // AsSlice returns the cache imports contents as a slice.
@@ -31,8 +52,8 @@ func (ci *CacheImports) AsSlice() []string {
 	ci.mu.RLock()
 	defer ci.mu.RUnlock()
 
-	clone := make([]string, len(ci.store))
-	copy(clone, ci.store)
+	clone := make([]string, len(ci.slice))
+	copy(clone, ci.slice)
 
 	return clone
 }
