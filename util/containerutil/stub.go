fd35d986e0664bc18df515a106780089ae47a42b
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2022-08-17 15:38:26 -0700

    Return errors in stub frontend.

--- util/containerutil/stub.go
+++ util/containerutil/stub.go
@@ -7,11 +7,14 @@ import (
 	"github.com/pkg/errors"
 )
 
-// This is a stub for use in internal testing when its too much effort to provide a legitimate backend.
+// This is a stub for use when a proper frontend is not available.
 // Should never be used IRL.
 type stubFrontend struct {
 	*shellFrontend
 }
+
+// FrontendNotInitializedError is returned when the frontend is not initialized.
+var FrontendNotInitializedError = errors.New("frontend (e.g. docker/podman) not initialized")
 
 // NewStubFrontend creates a stubbed frontend. Useful in cases where a frontend could not be detected, but we still need a frontend.
 // Examples include earthly/earthly, or integration tests. It is currently only used as a fallback when docker or other frontends are missing.
@@ -32,8 +35,8 @@ func NewStubFrontend(ctx context.Context, cfg *FrontendConfig) (ContainerFronten
 func (*stubFrontend) Scheme() string {
 	return ""
 }
-func (*stubFrontend) IsAvaliable(ctx context.Context) bool {
-	return true
+func (*stubFrontend) IsAvailable(ctx context.Context) bool {
+	return false
 }
 func (sf *stubFrontend) Config() *CurrentFrontend {
 	return &CurrentFrontend{
@@ -45,38 +48,38 @@ func (*stubFrontend) Information(ctx context.Context) (*FrontendInfo, error) {
 	return &FrontendInfo{}, nil
 }
 func (*stubFrontend) ContainerInfo(ctx context.Context, namesOrIDs ...string) (map[string]*ContainerInfo, error) {
-	return make(map[string]*ContainerInfo), nil
+	return nil, FrontendNotInitializedError
 }
 func (*stubFrontend) ContainerRemove(ctx context.Context, force bool, namesOrIDs ...string) error {
-	return nil
+	return FrontendNotInitializedError
 }
 func (*stubFrontend) ContainerStop(ctx context.Context, timeoutSec uint, namesOrIDs ...string) error {
-	return nil
+	return FrontendNotInitializedError
 }
 func (*stubFrontend) ContainerLogs(ctx context.Context, namesOrIDs ...string) (map[string]*ContainerLogs, error) {
-	return make(map[string]*ContainerLogs), nil
+	return nil, FrontendNotInitializedError
 }
 func (*stubFrontend) ContainerRun(ctx context.Context, containers ...ContainerRun) error {
-	return nil
+	return FrontendNotInitializedError
 }
 func (*stubFrontend) ImageInfo(ctx context.Context, refs ...string) (map[string]*ImageInfo, error) {
-	return make(map[string]*ImageInfo), nil
+	return nil, FrontendNotInitializedError
 }
 func (*stubFrontend) ImagePull(ctx context.Context, refs ...string) error {
-	return nil
+	return FrontendNotInitializedError
 }
 func (*stubFrontend) ImageRemove(ctx context.Context, force bool, refs ...string) error {
-	return nil
+	return FrontendNotInitializedError
 }
 func (*stubFrontend) ImageTag(ctx context.Context, tags ...ImageTag) error {
-	return nil
+	return FrontendNotInitializedError
 }
 func (*stubFrontend) ImageLoadFromFileCommand(filename string) string {
 	return ""
 }
 func (*stubFrontend) ImageLoad(ctx context.Context, image ...io.Reader) error {
-	return nil
+	return FrontendNotInitializedError
 }
 func (*stubFrontend) VolumeInfo(ctx context.Context, volumeNames ...string) (map[string]*VolumeInfo, error) {
-	return make(map[string]*VolumeInfo), nil
+	return nil, FrontendNotInitializedError
 }
