5bc31964536b5ba7b822def8327f22d07201a9ed
Ryan LaForge <rajlaforge@gmail.com>
2022-09-08 11:01:41 -0600

    feat: make typescript example into very simple express server

--- examples/typescript-node/app/index.ts
+++ examples/typescript-node/app/index.ts
@@ -1,5 +1,16 @@
-if (require.main === module) {
-    console.log('Hello Earthly!');
-} else {
-    console.log('required as a module');
-}
+import express from 'express';
+import {sayHello} from './server';
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    const who = req?.query?.who;
+    if (who && typeof who !== 'string') {
+        res.status(400).send(`provided who is not a string`);
+        return;
+    }
+    const hello = sayHello(who);
+    res.status(200).send(hello);
+});
+
+app.listen('8080', () => console.log(`Hello world app listening on port 8080`));
