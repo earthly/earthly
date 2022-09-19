6dd36add5882e158d088884643d07287d9c9f16e
Ryan LaForge <rajlaforge@gmail.com>
2022-09-19 11:07:20 -0600

    push up ensuring single installation command

--- .github/scripts/workflow.py
+++ .github/scripts/workflow.py
@@ -7,40 +7,73 @@ import io
 DOCKERHUB_MIRROR_USERNAME = os.environ.get('DOCKERHUB_MIRROR_USERNAME')
 DOCKERHUB_MIRROR_PASSWORD = os.environ.get('DOCKERHUB_MIRROR_PASSWORD')
 
+
+def run(run: str, **kwargs) -> (int, str):
+    output = io.StringIO()
+    cmd = pexpect.spawn(run, **kwargs)
+    cmd.logfile_read = output
+    status = cmd.wait()
+    s = ''.join(ch for ch in output.getvalue() if ch.isprintable() or ch == '\n')
+    return status, s
+
+
 class FrontendCommon:
     def _login(self, binary):
-        output = io.StringIO()
         print(f'running {binary} login')
-        cmd = pexpect.spawn(
+        status, output = run(
             f'{binary} login registry-1.docker.io.mirror.corp.earthly.dev --username "{DOCKERHUB_MIRROR_USERNAME}" --password "{DOCKERHUB_MIRROR_PASSWORD}"',
             encoding='utf-8')
-        cmd.logfile_read = output
-        status = cmd.wait()
-        print(f'{binary} finished')
+        print(f'{binary} login finished')
         if status:
             print(f'{binary} login failed with exit code {status} > ')
             print(f'===== {binary} login output =====')
-            s = ''.join(ch for ch in output.getvalue() if ch.isprintable() or ch == '\n')
-            print(s)
+            print(output)
             print(f'===== {binary} login output finished =====')
+
 
 class DockerWorkflowRunner(FrontendCommon):
     def login(self):
         super(DockerWorkflowRunner, self)._login("docker")
 
     def ensureSingleInstallation(self):
-        # todo: make sure only docker is installed?
-        pass
+        status, output = run("podman --version")
+        if status:
+            # Assume podman is NOT installed and return
+            return
+        # Uninstall podman, Assuming Ubuntu
+        status, output = run("apt-get purge podman -y")
+        if status == 0:
+            status, output = run("podman --version")
+            if status:
+                # podman uninstalled successfully
+                return
+            raise RuntimeError("still detected Podman after purge command")
+
+        raise RuntimeError(f"failed to uninstall Podman > {status} > {output}")
+
 
 class PodmanWorkflowRunner(FrontendCommon):
 
     def ensureSingleInstallation(self):
-        # todo: make sure only podman is installed?
-        pass
+        status, output = run("docker --version")
+        if status:
+            # Assume Docker is NOT installed and return
+            return
+        # Uninstall docker completely
+        status, output = run("apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli")
+        if status:
+            raise RuntimeError(f"failed to uninstall docker first step > {status} > {output}")
+        status, output = run("apt-get autoremove -y --purge docker-engine docker docker.io docker-ce")
+        if status:
+            raise RuntimeError(f"failed to uninstall docker second step > {status} > {output}")
+        status, output = run("docker --version")
+        if status:
+            # docker uninstalled successfully
+            return
+        raise RuntimeError(f"docker still detected after uninstall commands > {status} > {output}")
 
     def login(self):
         super(PodmanWorkflowRunner, self)._login("podman")
-
 
 
 if __name__ == "__main__":
