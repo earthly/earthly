607f904a37f02e298ea729d6831f7dd5ca68567a
Ryan LaForge <rajlaforge@gmail.com>
2022-09-19 12:34:24 -0600

    change all to manually sys.stdout.write

--- .github/scripts/workflow.py
+++ .github/scripts/workflow.py
@@ -8,27 +8,30 @@ DOCKERHUB_MIRROR_USERNAME = os.environ.get('DOCKERHUB_MIRROR_USERNAME')
 DOCKERHUB_MIRROR_PASSWORD = os.environ.get('DOCKERHUB_MIRROR_PASSWORD')
 
 
-def run(run: str, **kwargs) -> (int, str):
+def run(run: str, cmdName: str, **kwargs) -> (int, str):
+    sys.stdout.write(f'running {cmdName}')
     output = io.StringIO()
     cmd = pexpect.spawn(run, **kwargs)
     cmd.logfile_read = output
     status = cmd.wait()
+    sys.stdout.write(f'{cmdName} finished')
     s = ''.join(ch for ch in output.getvalue() if ch.isprintable() or ch == '\n')
+    if status:
+        sys.stdout.write(f'{cmdName} failed with exit code {status} > ')
+    else:
+        sys.stdout.write(f'{cmdName} exit code 0')
+    sys.stdout.write(f'===== {cmdName} login output =====')
+    sys.stdout.write(s)
+    sys.stdout.write(f'===== {cmdName} login output finished =====')
     return status, s
 
 
 class FrontendCommon:
     def _login(self, binary):
-        print(f'running {binary} login')
-        status, output = run(
+        run(
             f'{binary} login registry-1.docker.io.mirror.corp.earthly.dev --username "{DOCKERHUB_MIRROR_USERNAME}" --password "{DOCKERHUB_MIRROR_PASSWORD}"',
+            f'{binary} login',
             encoding='utf-8')
-        print(f'{binary} login finished')
-        if status:
-            print(f'{binary} login failed with exit code {status} > ')
-            print(f'===== {binary} login output =====')
-            print(output)
-            print(f'===== {binary} login output finished =====')
 
 
 class DockerWorkflowRunner(FrontendCommon):
@@ -36,14 +39,14 @@ class DockerWorkflowRunner(FrontendCommon):
         super(DockerWorkflowRunner, self)._login("docker")
 
     def ensureSingleInstallation(self):
-        status, output = run("podman --version")
+        status, output = run("podman --version", "podman --version")
         if status:
             # Assume podman is NOT installed and return
             return
         # Uninstall podman, Assuming Ubuntu
-        status, output = run("apt-get purge podman -y")
+        status, output = run("apt-get purge podman -y", "apt-get purge podman -y")
         if status == 0:
-            status, output = run("podman --version")
+            status, output = run("podman --version", "podman --version")
             if status:
                 # podman uninstalled successfully
                 return
@@ -77,25 +80,29 @@ class PodmanWorkflowRunner(FrontendCommon):
 
 
 if __name__ == "__main__":
-    sys.stdout.write("does this show?")
-    sys.exit(3)
-    if len(sys.argv) < 3:
-        raise RuntimeError("workflow.py must be called like: workflow.py {binary} {command}")
-    binary: str = sys.argv[1]
-    command: str = sys.argv[2]
-    runner = None
-    if binary.lower() == "docker":
-        runner = DockerWorkflowRunner()
-    elif binary.lower() == "podman":
-        runner = PodmanWorkflowRunner()
-    else:
-        raise RuntimeError(f"binary {binary} is invalid")
+    def work():
+        if len(sys.argv) < 3:
+            raise RuntimeError("workflow.py must be called like: workflow.py {binary} {command}")
+        binary: str = sys.argv[1]
+        command: str = sys.argv[2]
+        if binary.lower() == "docker":
+            runner = DockerWorkflowRunner()
+        elif binary.lower() == "podman":
+            runner = PodmanWorkflowRunner()
+        else:
+            raise RuntimeError(f"binary {binary} is invalid")
 
-    commands = {
-        "login": runner.login,
-        "ensureSingleInstallation": runner.ensureSingleInstallation
-    }
+        commands = {
+            "login": runner.login,
+            "ensureSingleInstallation": runner.ensureSingleInstallation
+        }
 
-    commandToRun = commands[command]
-    if commandToRun is None:
-        raise RuntimeError(f'got invalid command {commandToRun}')
+        commandToRun = commands[command]
+        if commandToRun is None:
+            raise RuntimeError(f'got invalid command {commandToRun}')
+
+    try:
+        work()
+    except Exception as e:
+        sys.stderr.write(f"failed to run cmd > {e}")
+
