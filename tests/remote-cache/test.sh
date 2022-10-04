7f775dc911412c521732e511586ae035f074629d
Ryan LaForge <rajlaforge@gmail.com>
2022-10-03 22:43:14 -0600

    [ci]: Patch the test-local and misc-test jobs

--- tests/remote-cache/test.sh
+++ tests/remote-cache/test.sh
@@ -22,8 +22,15 @@ earthly=${earthly-"../../build/linux/amd64/earthly"}
     -p "127.0.0.1:5000:5000" \
     --name registry registry:2
 
-export REGISTRY_IP="$(\"$frontend\" inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} registry)"
+export REGISTRY_IP="$($frontend inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} registry)"
 export REGISTRY="$REGISTRY_IP:5000"
+
+if test -z "$REGISTRY_IP"
+then
+     echo echo "Error: REGISTRY_IP is empty"
+     exit 4
+fi
+
 
 # Test.
 set +e
