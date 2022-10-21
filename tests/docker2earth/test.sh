3f4d3bd448610d0c80034a93781f2ffc8db011f9
Roberto Villarreal <rrjjvv@yahoo.com>
2022-10-20 18:12:04 -0600

    Improve shell constructs
    These changes weren't needed, but they cut down on IDE noise (warnings)

--- tests/docker2earth/test.sh
+++ tests/docker2earth/test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -eu
 
-earthly=`pwd`/build/linux/amd64/earthly
+earthly=$(pwd)/build/linux/amd64/earthly
 dockerfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo === Testing Dockerfile1 ===
-cd $(mktemp -d)
+echo "=== Testing Dockerfile1 ==="
+cd "$(mktemp -d)"
 echo "working out of $(pwd)"
-cp $dockerfiles/Dockerfile1 Dockerfile
+cp "$dockerfiles"/Dockerfile1 Dockerfile
 $earthly docker2earthly --tag=myimage:latest
 $earthly +build
 docker run --rm myimage:latest say-hi | grep hello
 
-echo === Testing Dockerfile2 ===
-cd $(mktemp -d)
+echo "=== Testing Dockerfile2 ==="
+cd "$(mktemp -d)"
 echo "working out of $(pwd)"
-cat $dockerfiles/Dockerfile2 | $earthly docker2earthly --dockerfile - --tag myotherimage:test
-cp $dockerfiles/app.go .
+cat "$dockerfiles"/Dockerfile2 | $earthly docker2earthly --dockerfile - --tag myotherimage:test
+cp "$dockerfiles"/app.go .
 $earthly +build
 docker run --rm myotherimage:test | grep greetings
