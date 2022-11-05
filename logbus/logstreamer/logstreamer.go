49651e29e76968cefd3b598a28cc1bef96e8b65c
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2022-11-05 14:50:59 -0700

    Minor improvements to make locking less likely.

--- logbus/logstreamer/logstreamer.go
+++ logbus/logstreamer/logstreamer.go
@@ -45,11 +45,77 @@ func New(ctx context.Context, bus *logbus.Bus, c cloud.Client, initialManifest *
 	return ls
 }
 
+const maxBatchSize = 128
+const maxBatchDuration = 200 * time.Millisecond
+
+func (ls *LogStreamer) retryLoop(ctx context.Context) {
+	defer close(ls.doneCh)
+	const maxRetry = 10
+	for i := 0; i < maxRetry; i++ {
+		retry, err := ls.tryStream(ctx)
+		if err == nil {
+			return
+		}
+		if i == maxRetry-1 {
+			retry = false
+		}
+		if !retry {
+			ls.errors = append(ls.errors, err)
+			return
+		}
+		fmt.Fprintf(os.Stderr, "transient error streaming logs: %v", err)
+	}
+}
+
+func (ls *LogStreamer) tryStream(ctx context.Context) (bool, error) {
+	ctxTry, cancelTry := context.WithCancel(ctx)
+	defer cancelTry()
+	ls.mu.Lock()
+	if ls.cancelled {
+		// TODO (vladaionescu): It would be nice if on cancellation we could
+		// 						still go through the entire retry loop.
+		//						This would require that we close ls.ch on each
+		//						retry somehow safely.
+		ls.mu.Unlock()
+		return false, errors.New("log streamer closed")
+	}
+	const chSize = 128
+	ls.ch = make(chan *logstream.Delta, chSize)
+	ls.batchedCh = make(chan []*logstream.Delta, chSize*maxBatchSize)
+	ls.mu.Unlock()
+	ls.batchedCh <- []*logstream.Delta{
+		{
+			DeltaTypeOneof: &logstream.Delta_DeltaManifest{
+				DeltaManifest: &logstream.DeltaManifest{
+					DeltaManifestOneof: &logstream.DeltaManifest_ResetAll{
+						ResetAll: ls.initialManifest,
+					},
+				},
+			},
+		},
+	}
+	go ls.batcherLoop(ctxTry)
+	ls.bus.AddSubscriber(ls)
+	defer ls.bus.RemoveSubscriber(ls)
+	err := ls.c.StreamLogs(ctxTry, ls.buildID, ls.batchedCh)
+	if err != nil {
+		s, ok := status.FromError(errors.Cause(err))
+		if !ok {
+			return false, err
+		}
+		switch s.Code() {
+		case codes.Unavailable, codes.Internal, codes.DeadlineExceeded:
+			return true, err
+		default:
+			return false, err
+		}
+	}
+	return false, nil
+}
+
 // batcherLoop batches deltas into batches of deltas, based on a maximum
 // batch size and a maximum delay interval.
 func (ls *LogStreamer) batcherLoop(ctx context.Context) {
-	const maxBatchSize = 128
-	const maxBatchDuration = 200 * time.Millisecond
 	ls.mu.Lock()
 	ch := ls.ch
 	batchedCh := ls.batchedCh
@@ -79,75 +145,10 @@ func (ls *LogStreamer) batcherLoop(ctx context.Context) {
 	}
 }
 
-func (ls *LogStreamer) retryLoop(ctx context.Context) {
-	defer close(ls.doneCh)
-	const maxRetry = 10
-	for i := 0; i < maxRetry; i++ {
-		retry, err := ls.tryStream(ctx)
-		ls.bus.RemoveSubscriber(ls)
-		if err == nil {
-			return
-		}
-		if i == maxRetry-1 {
-			retry = false
-		}
-		if !retry {
-			ls.errors = append(ls.errors, err)
-			return
-		}
-		fmt.Fprintf(os.Stderr, "transient error streaming logs: %v", err)
-	}
-}
-
-func (ls *LogStreamer) tryStream(ctx context.Context) (bool, error) {
-	ctxTry, cancelTry := context.WithCancel(ctx)
-	defer cancelTry()
-	ls.mu.Lock()
-	if ls.cancelled {
-		// TODO (vladaionescu): It would be nice if on cancellation we could
-		// 						still go through the entire retry loop.
-		//						This would require that we close ls.ch on each
-		//						retry somehow safely.
-		ls.mu.Unlock()
-		return false, errors.New("log streamer closed")
-	}
-	ls.ch = make(chan *logstream.Delta, 10240)
-	ls.batchedCh = make(chan []*logstream.Delta, 10240)
-	ls.mu.Unlock()
-	ls.batchedCh <- []*logstream.Delta{
-		{
-			DeltaTypeOneof: &logstream.Delta_DeltaManifest{
-				DeltaManifest: &logstream.DeltaManifest{
-					DeltaManifestOneof: &logstream.DeltaManifest_ResetAll{
-						ResetAll: ls.initialManifest,
-					},
-				},
-			},
-		},
-	}
-	go ls.batcherLoop(ctxTry)
-	ls.bus.AddSubscriber(ls)
-
-	err := ls.c.StreamLogs(ctxTry, ls.buildID, ls.batchedCh)
-	if err != nil {
-		s, ok := status.FromError(errors.Cause(err))
-		if !ok {
-			return false, err
-		}
-		switch s.Code() {
-		case codes.Unavailable, codes.Internal, codes.DeadlineExceeded:
-			return true, err
-		default:
-			return false, err
-		}
-	}
-	return false, nil
-}
-
 // Write writes the given delta to the log streamer.
 func (ls *LogStreamer) Write(delta *logstream.Delta) {
 	ls.mu.Lock()
-	ch := ls.ch
+	ch := ls.ch // ls.ch may get replaced on retry
 	ls.mu.Unlock()
 	ch <- delta
 }
