2831bd2a54b44a4f0a62cadec1e86d322c103b4f
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2022-11-16 14:25:26 -0800

    Don't expire when JWT set externally.

--- cloud/client.go
+++ cloud/client.go
@@ -136,6 +136,7 @@ func NewClient(httpAddr, grpcAddr string, useInsecure bool, agentSockPath, authC
 	}
 	if authJWTOverride != "" {
 		c.authToken = authJWTOverride
+		c.authTokenExpiry = time.Now().Add(24 * 365 * time.Hour) // Never expire when using JWT.
 	} else if authCredsOverride != "" {
 		c.authCredToken = authCredsOverride
 	} else {
