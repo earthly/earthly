ba1b82a8fdeb67a8e4f2af44e365bbabeb5dc2be
Ryan LaForge <rajlaforge@gmail.com>
2022-12-12 16:44:53 -0600

    [web-ui]: Fill out UI command - except JWT

--- cmd/earthly/web_cmds.go
+++ cmd/earthly/web_cmds.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"net/url"
 
+	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
 )
 
 func (app *earthlyApp) webUI(cliCtx *cli.Context) error {
+	urlToOpen, err := url.Parse(fmt.Sprintf("%s/login", app.cloudHTTPAddr))
+	if err != nil {
+		return errors.Wrapf(err, "failed to parse url")
+	}
+	if app.loginProvider != "" {
+		urlToOpen.Query().Set("provider", app.loginProvider)
+	}
+	if app.loginFinal != "" {
+		urlToOpen.Query().Set("final", app.loginFinal)
+	}
+
+	// TODO: Get token
+	var token string
+	if token != "" {
+		urlToOpen.Query().Set("token", token)
+	}
+
 	return errors.New("unimplemented")
 }
