26bbcbb0052e32613025925695f627da8a3b536c
Ryan LaForge <rajlaforge@gmail.com>
2022-12-14 10:43:11 -0600

    [web-ui]:  commit Earthfile and web.sh changes

--- scripts/tests/web.sh
+++ scripts/tests/web.sh
@@ -5,8 +5,8 @@ earthly=${earthly:=earthly}
 earthly=$(realpath "$earthly")
 echo "running tests with $earthly"
 
-## Test that we display web-ui link correctly without being logged in and with no params
-NO_COLOR=0 "$earthly" web-ui 2>&1 | grep -qE "https://ci(-beta)?.earthly.dev/login"
+## Test that we display web link correctly without being logged in and with no params
+NO_COLOR=0 "$earthly" web 2>&1 | grep -qE "https://ci(-beta)?.earthly.dev/login"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "failed without login and no arguments"
@@ -14,7 +14,7 @@ if [ $RESULT -ne 0 ]; then
 fi
 
 # Test with arguments passed for provider and final
-NO_COLOR=0 "$earthly" web-ui --provider github --final ci 2>output-file
+NO_COLOR=0 "$earthly" web --provider github --final ci 2>output-file
 output=$(cat output-file)
 rm output-file
 
@@ -41,7 +41,7 @@ echo Logging in
 "$earthly" account login
 
 # Test logged in - no args
-NO_COLOR=0 "$earthly" web-ui 2>&1 | grep -qE "https://ci(-beta)?.earthly.dev/login\?token=(.*)+"
+NO_COLOR=0 "$earthly" web 2>&1 | grep -qE "https://ci(-beta)?.earthly.dev/login\?token=(.*)+"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "failed with login and no arguments"
@@ -50,7 +50,7 @@ fi
 
 
 # Test logged in with arguments passed for provider and final
-NO_COLOR=0 "$earthly" web-ui --provider github --final ci 2>output-file
+NO_COLOR=0 "$earthly" web --provider github --final ci 2>output-file
 output=$(cat output-file)
 rm output-file
 
