fadea00fd7657e7df5eb639439d06e0e043c6165
dchw <corey@earthly.dev>
2023-04-04 11:27:22 -0600

    Earthly AMI release uses Amazon Linux 2

--- release/ami/install.sh
+++ release/ami/install.sh
@@ -2,35 +2,13 @@
 
 set -xe
 
-export DEBIAN_FRONTEND=noninteractive
+sudo yum-config-manager --add-repo https://pkg.earthly.dev/earthly.repo
+sudo yum update -y
+sudo amazon-linux-extras install docker
 
-sudo apt-get update
-sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common \
-    lsb-release
- 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
- 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkg.earthly.dev/earthly.pgp | sudo gpg --dearmor -o /etc/apt/keyrings/earthly-archive-keyring.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/earthly-archive-keyring.gpg] https://pkg.earthly.dev/deb \
-  stable main" | sudo tee /etc/apt/sources.list.d/earthly.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io earthly="$EARTHLY_VERSION"
+sudo yum install -y earthly
 
 docker -v
 earthly -v
 
-sudo usermod -a -G docker ubuntu
+sudo usermod -a -G docker ec2-user
