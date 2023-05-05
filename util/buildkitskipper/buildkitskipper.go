670f5a9011740cf29482a187f4d851275a8c9d6a
Alex Couture-Beil <alex@earthly.dev>
2023-05-05 14:43:53 -0700

    remove unneeded interface

--- util/buildkitskipper/buildkitskipper.go
+++
@@ -1,8 +0,0 @@
-package buildkitskipper
-
-import "context"
-
-type BuildkitSkipper interface {
-	Add(ctx context.Context, key []byte) error
-	Exists(ctx context.Context, key []byte) (bool, error)
-}
