650a89bf051f966262c4014732e0d42dadce7360
Alex Couture-Beil <alex@earthly.dev>
2023-07-13 10:16:00 -0700

    local-tests: add docker compose healthcheck
    `grep` is hiding failures, which makes it difficult to debug, instead
    move the grep to the fetch script, and use tee so we can see what is
    being returned.
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- tests/local/with-docker-compose-local-reg/fetch.sh
+++ tests/local/with-docker-compose-local-reg/fetch.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
+sleep 10
+output="$(mktemp)"
 curl --connect-timeout 1 \
     --max-time 1 \
     --retry 5 \
     --retry-delay 0 \
     --retry-max-time 10 \
-    "http://${WEBHOST}:${WEBPORT}"
+    "http://${WEBHOST}:${WEBPORT}" | tee "$output"
+echo "curl command worked"
+echo "checking $output contains the magic string"
+grep "hello world" "$output"
+echo "grep command worked"
