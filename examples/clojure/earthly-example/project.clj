49dbeee53a1056ea97fb6640358688092987521a
Velutas <simoneptas@gmail.com>
2023-07-14 09:01:29 -0700

    confirmed removing license okay

--- examples/clojure/earthly-example/project.clj
+++ examples/clojure/earthly-example/project.clj
@@ -1,8 +1,6 @@
 (defproject earthly-example "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot earthly-example.core
   :target-path "target/%s"
