77d512b383acad0a72c97a0e674438a6147e1a58
Ignacio del Valle Alles <idelvall@brutusin.org>
2023-09-15 18:45:35 +0200

    Update scripts/tests/export.sh
    Co-authored-by: Alex Couture-Beil <alex@earthly.dev>

--- scripts/tests/export.sh
+++ scripts/tests/export.sh
@@ -241,7 +241,7 @@ fi
 
 label_count=$("$frontend" inspect earthly-export-test-8b:test | jq .[].Config.Labels | grep -c dev.earthly.)
 if [ "$label_count" -ne "3" ]; then
-    echo "Expected 3 dev.earthly labels on second image"
+    echo "Expected 3 dev.earthly labels on second image; got $label_count"
     exit 1
 fi
 
