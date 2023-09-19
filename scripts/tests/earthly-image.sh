662e53a3b796028becbdc856c495edf9386a2314
Alex Couture-Beil <alex@earthly.dev>
2023-09-19 12:20:21 -0700

    split up sat tests from earthly-image tests
    Move the satellite related tests from earthly-image.sh into a separate
    earthly-image-sat.sh test, to make it make it more clear which tests
    depend on satellites or not.
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- scripts/tests/earthly-image.sh
+++ scripts/tests/earthly-image.sh
@@ -18,13 +18,6 @@ fi
 test -n "$DOCKERHUB_MIRROR_USERNAME" || (echo "error: DOCKERHUB_MIRROR_USERNAME is not set" && exit 1)
 test -n "$DOCKERHUB_MIRROR_PASSWORD" || (echo "error: DOCKERHUB_MIRROR_PASSWORD is not set" && exit 1)
 
-if [ -z "${EARTHLY_TOKEN:-}" ]; then
-  echo "using EARTHLY_TOKEN from earthly secrets"
-  EARTHLY_TOKEN="$(earthly secrets --org earthly-technologies --project core get earthly-token-for-satellite-tests)"
-  export EARTHLY_TOKEN
-fi
-test -n "$EARTHLY_TOKEN" || (echo "error: EARTHLY_TOKEN is not set" && exit 1)
-
 ENCODED_AUTH="$(echo -n "$DOCKERHUB_MIRROR_USERNAME:$DOCKERHUB_MIRROR_PASSWORD" | base64 -w 0)"
 
 dockerconfig="$(mktemp /tmp/earthly-image-test-docker-config.XXXXXX)"
@@ -87,14 +80,6 @@ acbgrep "Executes Earthly builds" output.txt # Display help
 "$FRONTEND" run --rm -e NO_BUILDKIT=1 "${EARTHLY_IMAGE}" --help 2>&1 | tee output.txt
 acbgrep "Executes Earthly builds" output.txt # Display help
 
-# TODO move satellite tests into a separate test (since they are flakey)
-
-echo "Test earthly sat inspect."
-"$FRONTEND" run --rm --privileged -e EARTHLY_TOKEN="${EARTHLY_TOKEN}" "${EARTHLY_IMAGE}" sat --org earthly-technologies inspect core-test 2>&1 | tee output.txt
-acbgrep "core-test" output.txt
-"$FRONTEND" run --rm -e EARTHLY_TOKEN="${EARTHLY_TOKEN}" -e NO_BUILDKIT=1 "${EARTHLY_IMAGE}" sat --org earthly-technologies inspect core-test 2>&1 | tee output.txt
-acbgrep "core-test" output.txt
-
 echo "Test hello world with embedded buildkit."
 "$FRONTEND" run --rm --privileged -e EARTHLY_ADDITIONAL_BUILDKIT_CONFIG -v "$dockerconfig:/root/.docker/config.json" "${EARTHLY_IMAGE}" --no-cache github.com/earthly/hello-world:4d466d524f768a379374c785fdef30470e87721d+hello 2>&1 | tee output.txt
 acbgrep "Hello World" output.txt
@@ -106,30 +91,6 @@ if [ "$FRONTEND" = "docker" ]; then
     acbgrep "Hello World" output.txt
     acbgrep "Earthly installation is working correctly" output.txt
 fi
-
-echo "Test satellite (not privileged, no buildkit)."
-
-satconfig="$(mktemp /tmp/earthly-image-test-satellite-config.XXXXXX)"
-chmod 600 "$satconfig"
-if [ -n "${DOCKERHUB_USERNAME:-}" ] && [ -n "${DOCKERHUB_PASSWORD:-}" ]; then
-    ENCODED_SAT_AUTH="$(echo -n "$DOCKERHUB_USERNAME:$DOCKERHUB_PASSWORD" | base64 -w 0)"
-
-    cat > "$satconfig" <<EOF
-{
-	"auths": {
-		"docker.io": {
-			"auth": "$ENCODED_SAT_AUTH"
-		}
-	}
-}
-EOF
-fi
-
-# TODO FIXME test that the above credentials are actually used by the satellite
-
-"$FRONTEND" run --rm -e EARTHLY_TOKEN="${EARTHLY_TOKEN}" -v "$satconfig:/root/.docker/config.json" -e NO_BUILDKIT=1 "${EARTHLY_IMAGE}" --org earthly-technologies --sat core-test --no-cache github.com/earthly/hello-world:4d466d524f768a379374c785fdef30470e87721d+hello 2>&1 | tee output.txt
-acbgrep "Hello World" output.txt
-acbgrep "Earthly installation is working correctly" output.txt
 
 rm output.txt
 echo "=== All tests have passed ==="
