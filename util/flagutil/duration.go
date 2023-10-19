991cdf8e9825a4b81aa25c3c9d850f5e5a37f77a
Ido David <ido@earthly.dev>
2023-10-19 13:18:27 -0400

    split custom (generic) flag types to separate files

--- util/flagutil/duration.go
+++ util/flagutil/duration.go
@@ -5,22 +5,7 @@ import (
 	"fmt"
 	"strings"
 	"time"
-
-	"github.com/dustin/go-humanize"
 )
-
-type ByteSizeValue uint64
-
-func (b *ByteSizeValue) Set(s string) error {
-	v, err := humanize.ParseBytes(s)
-	if err != nil {
-		return err
-	}
-	*b = ByteSizeValue(v)
-	return nil
-}
-
-func (b *ByteSizeValue) String() string { return humanize.Bytes(uint64(*b)) }
 
 // Duration implements cli.GenericFlag methods to support time.Duration with days, e.g. 1d
 type Duration time.Duration
