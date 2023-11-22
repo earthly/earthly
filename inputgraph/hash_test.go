a97bb67c4f79a6084a6b2081adddff4752f4055e
Mike <mikejholly@gmail.com>
2023-11-21 17:18:23 -0800

    Auto-skip: support FROM DOCKERFILE & remotes for WITH DOCKER --load

--- inputgraph/hash_test.go
+++ inputgraph/hash_test.go
@@ -128,3 +128,23 @@ func TestHashTargetWithDockerNoAlias(t *testing.T) {
 	hex := fmt.Sprintf("%x", hash)
 	r.Equal("6aaabf6d323280d4fd23960a4ba2b159dcfb84fd", hex)
 }
+
+func TestHashTargetWithDockerRemote(t *testing.T) {
+	r := require.New(t)
+	target := domain.Target{
+		LocalPath: "./testdata/with-docker",
+		Target:    "with-docker-load-remote",
+	}
+
+	ctx := context.Background()
+	cons := conslogging.New(os.Stderr, &sync.Mutex{}, conslogging.NoColor, 0, conslogging.Info)
+
+	hashOpt := HashOpt{Console: cons, Target: target}
+	org, project, hash, err := HashTarget(ctx, hashOpt)
+	r.NoError(err)
+	r.Equal("earthly-technologies", org)
+	r.Equal("core", project)
+
+	hex := fmt.Sprintf("%x", hash)
+	r.NotEmpty(hex)
+}
