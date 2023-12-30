b59377b031a577767e0952ea908b3355913e9268
Mike <mikejholly@gmail.com>
2023-12-30 14:42:58 -0800

    Guard against empty COPY sources

--- earthfile2llb/interpreter.go
+++ earthfile2llb/interpreter.go
@@ -922,7 +922,6 @@ func (i *Interpreter) handleCopy(ctx context.Context, cmd spec.Command) error {
 	if err != nil {
 		return i.wrapError(err, cmd.SourceLocation, "parse platform %s", expandedPlatform)
 	}
-
 	allClassical := true
 	allArtifacts := true
 	for index, src := range srcs {
@@ -972,7 +971,11 @@ func (i *Interpreter) handleCopy(ctx context.Context, cmd spec.Command) error {
 	if !allClassical && !allArtifacts {
 		return i.errorf(cmd.SourceLocation, "combining artifacts and build context arguments in a single COPY command is not allowed: %v", srcs)
 	}
-
+	for _, src := range srcs {
+		if src == "" {
+			return i.wrapError(err, cmd.SourceLocation, "empty COPY source encountered")
+		}
+	}
 	if slices.ContainsFunc(strings.Split(dest, "/"), func(s string) bool {
 		return s == "~" || strings.HasPrefix(s, "~")
 	}) {
