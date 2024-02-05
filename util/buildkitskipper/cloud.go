6830b2ed2f0cdd98ae9c2c8ef14fecf287ff7d94
Mike <mikejholly@gmail.com>
2024-02-05 14:53:05 -0800

    Lint fix & comments

--- util/buildkitskipper/cloud.go
+++ util/buildkitskipper/cloud.go
@@ -6,22 +6,27 @@ import (
 	"strings"
 )
 
+// ASKVClient provides methods which allow for adding & checking the existence
+// of an auto-skip hash. The current implementations are a local BoltDB & a
+// remote gRPC service.
 type ASKVClient interface {
 	AutoSkipExists(ctx context.Context, org string, hash []byte) (bool, error)
 	AutoSkipAdd(ctx context.Context, org, path, target string, hash []byte) error
 }
 
+// NewCloud creates and returns a new Cloud API implementation.
 func NewCloud(client ASKVClient) (*CloudClient, error) {
 	return &CloudClient{
 		client: client,
 	}, nil
 }
 
+// CloudClient implements the Cloud API version of the ASKVClient.
 type CloudClient struct {
-	org    string
 	client ASKVClient
 }
 
+// Add a new hash to the Cloud DB.
 func (c *CloudClient) Add(ctx context.Context, org, target string, data []byte) error {
 	parts := strings.Split(target, "+")
 	if len(parts) < 2 {
@@ -30,6 +35,7 @@ func (c *CloudClient) Add(ctx context.Context, org, target string, data []byte) 
 	return c.client.AutoSkipAdd(ctx, org, parts[0], parts[1], data)
 }
 
+// Exists checks if an auto-skip hash exists.
 func (c *CloudClient) Exists(ctx context.Context, org string, data []byte) (bool, error) {
 	return c.client.AutoSkipExists(ctx, org, data)
 }
