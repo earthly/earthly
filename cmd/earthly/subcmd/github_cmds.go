f692cacbd41193c4b314eb7b216c095ec55974f6
nacho <idelvall@brutusin.org>
2024-04-11 10:14:01 +0200

    address feedback and allow reading token from stdin

--- cmd/earthly/subcmd/github_cmds.go
+++ cmd/earthly/subcmd/github_cmds.go
@@ -2,6 +2,8 @@ package subcmd
 
 import (
 	"fmt"
+	"os/signal"
+	"syscall"
 
 	"github.com/urfave/cli/v2"
 
@@ -11,9 +13,10 @@ import (
 type Github struct {
 	cli CLI
 
-	Token  string
-	GHOrg  string
-	GHRepo string
+	Org     string
+	GHOrg   string
+	GHRepo  string
+	GHToken string
 }
 
 func NewGithub(cli CLI) *Github {
@@ -33,27 +36,32 @@ func (a *Github) Cmds() []*cli.Command {
 				{
 					Name:  "add",
 					Usage: "Add GHA integration",
-					Description: `This command sets the configuration to create a new Github-Earthly integration to trigger satellite builds from GHA (GitHub Actions).
-Integration can be done at two levels: org-wide and per repository. 
-The provided token must have enough permissions to register webhook and to create Github self hosted runners in those two different scenarios.`,
+					Description: `This command sets the configuration to create a new Github-Earthly integration, to trigger satellite builds from GHA (GitHub Actions).
+From the Github side, integration can be done at two levels: organization-wide and per repository. 
+The provided token must have enough permissions to register webhooks and to create Github self hosted runners in those two scenarios.`,
 					UsageText: "earthly github add --org <org> [--repo <repo>] --token <token>",
 					Action:    a.actionAdd,
 					Flags: []cli.Flag{
 						&cli.StringFlag{
 							Name:        "org",
+							Usage:       "The name of the Earthly organization to set an integration with. Defaults to selected organization",
+							Destination: &a.Org,
+						},
+						&cli.StringFlag{
+							Name:        "gh-org",
 							Usage:       "The name of the GitHub organization to set an integration with",
 							Required:    true,
 							Destination: &a.GHOrg,
 						},
 						&cli.StringFlag{
-							Name:        "repo",
+							Name:        "gh-repo",
 							Usage:       "The name of the GitHub repository to set an integration with",
 							Destination: &a.GHRepo,
 						},
 						&cli.StringFlag{
-							Name:        "token",
+							Name:        "gh-token",
 							Usage:       "The GitHub token used for the integration",
-							Destination: &a.Token,
+							Destination: &a.GHToken,
 						},
 					},
 				},
@@ -64,14 +72,34 @@ The provided token must have enough permissions to register webhook and to creat
 
 func (a *Github) actionAdd(cliCtx *cli.Context) error {
 	a.cli.SetCommandName("githubAdd")
+	if a.Org == "" {
+		if a.cli.OrgName() == "" {
+			return fmt.Errorf("coudn't determine Earthly organization")
+		}
+		a.Org = a.cli.OrgName()
+	}
+	if a.GHToken == "" {
+		// Our signal handling under main() doesn't cause reading from stdin to cancel
+		// as there's no way to pass app.ctx to stdin read calls.
+		signal.Reset(syscall.SIGINT, syscall.SIGTERM)
+		token, err := promptToken()
+		if err != nil {
+			return err
+		}
+		a.GHToken = token
+	}
 	cloudClient, err := helper.NewCloudClient(a.cli)
 	if err != nil {
 		return err
 	}
-	err = cloudClient.SetGithubToken(cliCtx.Context, a.cli.OrgName(), a.GHOrg, a.GHRepo, a.Token)
+	err = cloudClient.SetGithubToken(cliCtx.Context, a.cli.OrgName(), a.GHOrg, a.GHRepo, a.GHToken)
 	if err != nil {
 		return fmt.Errorf("error found running github add: %w", err)
 	}
 	a.cli.Console().Printf("GitHub integration successfully created")
 	return nil
+}
+
+func promptToken() (string, error) {
+	return promptHiddenText("Enter GH token")
 }
