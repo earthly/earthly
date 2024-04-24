7ada74e09bcaf31f44f866ae299ed1e1ec22bb6b
Adam Gordon Bell <adam@corecursive.com>
2024-04-24 14:44:26 -0400

    Update cmd/earthly/flag/global.go
    Co-authored-by: idodod <ido@earthly.dev>

--- cmd/earthly/flag/global.go
+++ cmd/earthly/flag/global.go
@@ -542,7 +542,7 @@ func (global *Global) RootFlags(installName string, bkImage string) []cli.Flag {
 		},
 		&cli.BoolFlag{
 			Name:        "github-annotations",
-			EnvVars:     []string{"GITHUB_ACTIONS"},
+			EnvVars:     []string{"GITHUB_ANNOTATIONS"},
 			Usage:       "Enable Git Hub Actions workflow specific output",
 			Destination: &global.IsGitHubActions,
 			Value:       false,
