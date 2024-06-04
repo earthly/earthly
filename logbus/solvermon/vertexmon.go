3c85619d046dad64734dc18b4c880aa080494687
Adam Gordon Bell <adam@corecursive.com>
2024-06-04 08:47:25 -0400

    wip

--- logbus/solvermon/vertexmon.go
+++ logbus/solvermon/vertexmon.go
@@ -38,8 +38,121 @@ type vertexMonitor struct {
 }
 
 var reErrExitCode = regexp.MustCompile(`^(?:process ".*" did not complete successfully|error calling LocalhostExec): exit code: (?P<exit_code>[0-9]+)$`)
+
+func getExitCode(errString string) (uint64, bool) {
+	if reErrExitCode.MatchString(errString) {
+		matches, _ := stringutil.NamedGroupMatches(errString, reErrExitCode)
+		exitCodeMatch := matches["exit_code"][0]
+		exitCode, err := strconv.ParseUint(exitCodeMatch, 10, 32)
+		if err != nil {
+			return 0, false
+		}
+		return exitCode, true
+	}
+	return 0, false
+}
+
 var reErrNotFound = regexp.MustCompile(`^failed to calculate checksum of ref ([^ ]*): (.*)$`)
 var reHint = regexp.MustCompile(`^(?P<msg>.+?):Hint: .+`)
+
+func determineFatalErrorType(errString string, exitCode uint64) (logstream.FailureType, bool) {
+	if strings.Contains(errString, "context canceled") || errString == "no active sessions" {
+		return 0, false
+	}
+	if reErrExitCode.MatchString(errString) {
+		switch exitCode {
+		case math.MaxUint32:
+			return logstream.FailureType_FAILURE_TYPE_OOM_KILLED, true
+		default:
+			return logstream.FailureType_FAILURE_TYPE_NONZERO_EXIT, true
+		}
+	}
+	if reErrNotFound.MatchString(errString) {
+		return logstream.FailureType_FAILURE_TYPE_FILE_NOT_FOUND, true
+	}
+	if strings.Contains(errString, errutil.EarthlyGitStdErrMagicString) {
+		return logstream.FailureType_FAILURE_TYPE_GIT, true
+	}
+	return logstream.FailureType(0), false
+}
+
+func formatError(errString string, fatalErrorType logstream.FailureType, exitCode uint64, indentOp string, internalStr string) string {
+	if matches, _ := stringutil.NamedGroupMatches(errString, reHint); len(matches["msg"]) == 1 {
+		errString = matches["msg"][0]
+	}
+	formattedError := ""
+	switch fatalErrorType {
+	case logstream.FailureType_FAILURE_TYPE_OOM_KILLED:
+		formattedError = fmt.Sprintf(""+
+			"      The%s command\n"+
+			"          %s\n"+
+			"      was terminated because the build system ran out of memory. If you are using a satellite or other remote buildkit, it is the remote system that ran out of memory.",
+			internalStr, indentOp)
+	case logstream.FailureType_FAILURE_TYPE_NONZERO_EXIT:
+		formattedError = fmt.Sprintf(""+
+			"      The%s command\n"+
+			"          %s\n"+
+			"      did not complete successfully. Exit code %d",
+			internalStr, indentOp, exitCode)
+	case logstream.FailureType_FAILURE_TYPE_FILE_NOT_FOUND:
+		m := reErrNotFound.FindStringSubmatch(errString)
+		formattedError = fmt.Sprintf(""+
+			"      The%s command\n"+
+			"          %s\n"+
+			"      failed: %s",
+			internalStr, indentOp, m[2])
+	case logstream.FailureType_FAILURE_TYPE_GIT:
+		gitStdErr, shorterErr, ok := errutil.ExtractEarthlyGitStdErr(errString)
+		if ok {
+			formattedError = fmt.Sprintf("The%s command\n          %s\nfailed: %s\n\n%s", internalStr, indentOp, shorterErr, gitStdErr)
+		} else {
+			formattedError = fmt.Sprintf("The%s command\n          %s\nfailed: %s", internalStr, indentOp, errString)
+		}
+	default:
+		formattedError = fmt.Sprintf("The%s command\n          %s\nfailed: %s", internalStr, indentOp, errString)
+	}
+	return formattedError
+}
+
+func FormatError(errString string) string {
+	exitCode, _ := getExitCode(errString)
+	fatalErrorType, _ := determineFatalErrorType(errString, exitCode)
+	return formatError(errString, fatalErrorType, exitCode, "", "")
+}
+
+func (vm *vertexMonitor) parseError() {
+	errString := vm.vertex.Error
+
+	// Add operation context to the error string
+	indentOp := strings.Join(strings.Split(vm.operation, "\n"), "\n          ")
+	internalStr := ""
+	if vm.meta.Internal {
+		internalStr = " internal"
+	}
+	errString = fmt.Sprintf("%s%s", internalStr, errString)
+
+	exitCode, _ := getExitCode(errString)
+	fatalErrorType, isFatalError := determineFatalErrorType(errString, exitCode)
+	formattedError := formatError(errString, fatalErrorType, exitCode, indentOp, internalStr)
+
+	// Add source location if available
+	slString := ""
+	if vm.meta.SourceLocation != nil {
+		slString = fmt.Sprintf(
+			" %s:%d:%d",
+			vm.meta.SourceLocation.File, vm.meta.SourceLocation.StartLine,
+			vm.meta.SourceLocation.StartColumn)
+	}
+
+	// Set the error string and flags on the vertexMonitor
+	if isFatalError {
+		vm.errorStr = fmt.Sprintf("ERROR%s\n%s", slString, formattedError)
+	} else {
+		vm.errorStr = fmt.Sprintf("WARN%s: %s", slString, formattedError)
+	}
+	vm.isFatalError = isFatalError
+	vm.fatalErrorType = fatalErrorType
+}
 
 func (vm *vertexMonitor) Write(dt []byte, ts time.Time, stream int) (int, error) {
 	if stream == BuildkitStatsStream {
@@ -64,84 +177,4 @@ func (vm *vertexMonitor) Write(dt []byte, ts time.Time, stream int) (int, error)
 		return 0, errors.Wrap(err, "write log line")
 	}
 	return len(dt), nil
-}
-
-func (vm *vertexMonitor) parseError() {
-	errString := vm.vertex.Error
-	indentOp := strings.Join(strings.Split(vm.operation, "\n"), "\n          ")
-	internalStr := ""
-	if vm.meta.Internal {
-		internalStr = " internal"
-	}
-	if matches, _ := stringutil.NamedGroupMatches(errString, reHint); len(matches["msg"]) == 1 {
-		errString = matches["msg"][0]
-	}
-	switch {
-	case strings.Contains(errString, "context canceled"):
-		vm.isCanceled = true
-		vm.errorStr = "WARN: Canceled"
-		return
-	case reErrExitCode.MatchString(errString):
-		matches, _ := stringutil.NamedGroupMatches(errString, reErrExitCode)
-		exitCodeMatch := matches["exit_code"][0]
-
-		// Ignore the parse error as default case will print it as a string using
-		// the source, so we won't miss any data.
-		exitCode, _ := strconv.ParseUint(exitCodeMatch, 10, 32)
-		switch exitCode {
-		case math.MaxUint32:
-			errString = fmt.Sprintf(""+
-				"      The%s command\n"+
-				"          %s\n"+
-				"      was terminated because the build system ran out of memory.\n"+
-				"      If you are using a satellite or other remote buildkit, it is the remote system that ran out of memory.",
-				internalStr, indentOp)
-			vm.fatalErrorType = logstream.FailureType_FAILURE_TYPE_OOM_KILLED
-		default:
-			errString = fmt.Sprintf(""+
-				"      The%s command\n"+
-				"          %s\n"+
-				"      did not complete successfully. Exit code %s",
-				internalStr, indentOp, exitCodeMatch)
-			vm.fatalErrorType = logstream.FailureType_FAILURE_TYPE_NONZERO_EXIT
-		}
-		vm.isFatalError = true
-	case reErrNotFound.MatchString(errString):
-		m := reErrNotFound.FindStringSubmatch(errString)
-		errString = fmt.Sprintf(""+
-			"      The%s command\n"+
-			"          %s\n"+
-			"      failed: %s",
-			internalStr, indentOp, m[2])
-		vm.isFatalError = true
-		vm.fatalErrorType = logstream.FailureType_FAILURE_TYPE_FILE_NOT_FOUND
-	case errString == "no active sessions":
-		vm.isCanceled = true
-		errString = "WARN: Canceled"
-	case strings.Contains(errString, errutil.EarthlyGitStdErrMagicString):
-		gitStdErr, shorterErr, ok := errutil.ExtractEarthlyGitStdErr(errString)
-		if ok {
-			errString = fmt.Sprintf(
-				"The%s command '%s' failed: %s\n\n%s", internalStr, vm.operation, shorterErr, gitStdErr)
-		} else {
-			errString = fmt.Sprintf(
-				"The%s command '%s' failed: %s", internalStr, vm.operation, errString)
-		}
-		vm.isFatalError = true
-	default:
-		errString = fmt.Sprintf(
-			"The%s command '%s' failed: %s", internalStr, vm.operation, errString)
-	}
-	slString := ""
-	if vm.meta.SourceLocation != nil {
-		slString = fmt.Sprintf(
-			" %s:%d:%d",
-			vm.meta.SourceLocation.File, vm.meta.SourceLocation.StartLine,
-			vm.meta.SourceLocation.StartColumn)
-	}
-	if vm.isFatalError {
-		vm.errorStr = fmt.Sprintf("ERROR%s\n%s", slString, errString)
-	} else {
-		vm.errorStr = fmt.Sprintf("WARN%s: %s", slString, errString)
-	}
 }
