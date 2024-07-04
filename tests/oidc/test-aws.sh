ae9da0adbdc20f65323ea4d25db52f2c3e9eca7f
Kenneth Tan <i.am@kennethtan.xyz>
2024-07-04 15:53:47 +0800

    fix failing tests due to missing --allow-privileged flag

--- tests/oidc/test-aws.sh
+++ tests/oidc/test-aws.sh
@@ -13,4 +13,4 @@ echo "== it should access aws via oidc =="
 earthly --config "$earthly_config" +oidc --ROLE_ARN="$ROLE_ARN"
 
 echo "== it should access aws via oidc-with-docker =="
-earthly --config "$earthly_config" +oidc-with-docker --ROLE_ARN="$ROLE_ARN"
+earthly --config "$earthly_config" --allow-privileged +oidc-with-docker --ROLE_ARN="$ROLE_ARN"
