a358c19c729f2ff1433b848ac3a7e85c57012453
Corey Larson <corey@earthly.dev>
2024-07-12 10:41:13 -0600

    Try to use a user-defined network

--- tests/registry-certs/test.sh
+++ tests/registry-certs/test.sh
@@ -15,10 +15,14 @@ test -n "$frontend" || (>&2 echo "Error: frontend is empty" && exit 1)
 # Cleanup previous run.
 "$frontend" stop registry || true
 "$frontend" rm registry || true
+"$frontend" rm network || true
 rm -rf "$testdir/certs" || true
 
+# Create user defined network.
+"$frontend" network create -d bridge registry-certs
+
 # Start registry to get its IP address.
-"$frontend" run --rm -d --name registry registry:2
+"$frontend" run --rm -d --network registry-certs --name registry registry:2
 export REGISTRY_IP="$("$frontend" inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} registry)"
 export REGISTRY="$REGISTRY_IP"
 "$frontend" stop registry
@@ -31,6 +35,7 @@ export REGISTRY="$REGISTRY_IP"
 
 # Run registry. This will use the same IP address as allocated above.
 "$frontend" run --rm -d \
+    --network registry-certs \
     --ip "$REGISTRY_IP" \
     -v "$testdir"/certs:/certs \
     -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
